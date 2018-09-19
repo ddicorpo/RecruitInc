e875f5eecb9eb80563da5f90eca2484ab24dc5d4
DESKTOP-9KJ158Q\James <23jamesff@gmail.com>
2018-09-19 01:15:18 -0400

    #1 added a basic logger with winston

--- BackEnd/index.js
+++ BackEnd/index.js
@@ -1,9 +1,42 @@
 // Warning: This code is temporary we will soon
 // include TypeScript...
+'use strict';
 require('dotenv').load();
 const express  = require('express');
 const app = express();
 const fetch = require('node-fetch');
+const winston = require('winston'); //library for the logger
+const fs = require('fs');
+const env = process.env.NODE_ENV || 'development';
+const logDir = 'log';
+
+
+// Create the log directory if it does not exist
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
+const tsFormat = () => (new Date()).toLocaleTimeString();
+//creates the logger
+const logger = winston.createLogger({
+    //each transport is a different directory to display or store a log
+    transports: [
+        // colorize the output to the console
+        new (winston.transports.Console)({
+            timestamp: tsFormat,
+            colorize: true,
+            level: 'info'
+        }),
+        new (winston.transports.File)({
+            filename: `${logDir}/results.log`,
+            timestamp: tsFormat,
+            level: env === 'development' ? 'debug' : 'info'
+        })
+    ]
+});
+//simple test to check for functionality of the logger
+logger.info('Hello world');
+logger.warn('Warning message');
+logger.debug('Debugging info');
 // Allowing Communication between our nodes...
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
