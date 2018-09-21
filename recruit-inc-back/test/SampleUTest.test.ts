8d24fac671f47ff9235a67e95eea6c8a1296ba6d
winterhart <m.user.work@gmail.com>
2018-09-20 20:50:18 -0400

    Coverage and nyc_output : removal of these directory, gitignore: addition of coverage and nyc, Addition of SampleWithMock mocking example

--- recruit-inc-back/test/SampleUTest.test.ts
+++ recruit-inc-back/test/SampleUTest.test.ts
@@ -1,7 +1,8 @@
 import { expect } from 'chai';
 import { Examples }  from '../src/routes/examples';
+import {} from 'mocha'
 
-describe('calculate', function () {
+describe('Compute Sum', function () {
     it('add', function () {
         var tmp = new Examples()
         let result = tmp.sum(5, 2);
