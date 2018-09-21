598c2f8c6233b0a11e02e2d5d8fde2e21ffd5aba
winterhart <m.user.work@gmail.com>
2018-09-21 00:32:16 -0400

    #1 Fix Error: Missing package Next in package.json, index.tsx: set an example for communication between two nodes

--- recruit-inc-front/pages/index.tsx
+++ recruit-inc-front/pages/index.tsx
@@ -1,9 +1,25 @@
 import * as React from 'react';
 import Header from '../components/Header'
 
-export default () => (
-	<div>
-		<Header />
-		<p>Hi from Index</p>
-	</div>
-)
+class App extends React.Component {
+
+	componentDidMount(){
+		fetch("http://localhost:6969/api/hi")
+			.then((response) => { return Promise.resolve(response.json()) })
+			.then(data => {
+				let items = data
+				console.log("state", items)
+			})
+	}
+	render(){
+		return (
+		<div>
+ 		<Header />;
+ 		<p>Hi from Index</p>
+		 </div>
+		);
+	}
+}
+
+
+export default App;
