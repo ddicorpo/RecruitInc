78a9a0614eda69b64fdf50d65a8c08a52bb649c5
momo2793 <mohamed_2_27@hotmail.com>
2018-09-21 00:07:39 -0400

    #1 Fixed bug for Languages, PR and Repositories data metric for Github extraction

--- recruit-inc-back/src/data-extraction/github/query.ts
+++ recruit-inc-back/src/data-extraction/github/query.ts
@@ -9,46 +9,27 @@ export class Query {
 
     async getData(username: string): Promise<string> {
         let query : string =
-            `query {
-                user(login: "${username}") {
-                  repositories(first: 10, isFork: false) {
-                    nodes {
-                      name
-                      url
-                        
-                      languages(first:5) {
-                        nodes {
-                          name
-                          
-                        }
-
-                        pullRequest(number: 2) {
-                            id
-                            number
-                            title
-                            
-                          }
-
-                          issues(last:20, states:CLOSED) {
-                            edges {
-                              node {
-                                title
-                                url
-                                labels(first:5) {
-                                  edges {
-                                    node {
-                                      name
-                                    }
-                                  }
-                                }
-                              }
-                            }
-                          }
-                      }
-                    }
-                  }
-                }
-              }`;
+            `query
+            {
+             user(login: "${username}") {
+               repositories(first: 10, isFork: false) {
+                 nodes {
+                   name
+                   url
+                   languages(first: 5) {
+                     nodes {
+                       name
+                     }
+                   }
+                   pullRequest(number: 2) {
+                     id
+                     number
+                     title
+                   }
+                 }
+               }
+             }
+            } `;
 
         return await new GithubApiV4().queryData(this.accessToken, query);
     }
