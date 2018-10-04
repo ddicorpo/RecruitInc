46e21ea161ab995c8577de6b9bf55ce17cb14f29
winterhart <m.user.work@gmail.com>
2018-10-04 17:27:21 -0400

    Removal of autogenerated JS file, (task-Wiki), Push of a new branch related to #29

--- recruit-inc-front/pages/about.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const React = require("react");
-const Header_1 = require("../components/Header");
-exports.default = () => (React.createElement("div", null,
-    React.createElement(Header_1.default, null),
-    React.createElement("p", null, "Hi from about")));
