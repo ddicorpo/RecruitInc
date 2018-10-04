08651b313fea79225c3c0dd7c8689d8b6a91f946
BenjaminTanguay <benjamin.tanguay@gmail.com>
2018-10-04 00:16:20 -0400

    #25 Created an example request to demonstrate the gitlab api

--- recruit-inc-back/src/app.ts
+++ recruit-inc-back/src/app.ts
@@ -2,18 +2,21 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser'; //used to parse the form data that you pass in the request
 import { Examples } from "./routes/examples";
 import { Applicant } from "./routes/github/applicant";
+import { GitlabApplicants } from "./routes/gitlab/GitlabApplicants"
 
 class App {
 
     public app: express.Application;
     public myDataRoute: Examples = new Examples();
     public applicantDataRoute: Applicant = new Applicant();
+    public gitlabApplicant: GitlabApplicants = new GitlabApplicants();
 
     constructor() {
         this.app = express(); //run the express instance and store in app
         this.config();
         this.myDataRoute.routes(this.app);
         this.applicantDataRoute.routes(this.app);
+        this.gitlabApplicant.routes(this.app);
     }
 
     private config(): void {
