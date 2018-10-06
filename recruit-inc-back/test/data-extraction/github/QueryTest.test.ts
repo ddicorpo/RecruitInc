d1fa251a953354c1ac96cd8d9cb4390155dc9aa6
aghiles <aghiles_1@hotmail.com>
2018-10-06 12:31:07 -0400

    #19 adjustments on the tests

--- recruit-inc-back/test/data-extraction/github/QueryTest.test.ts
+++ recruit-inc-back/test/data-extraction/github/QueryTest.test.ts
@@ -1,79 +1,61 @@
 import { expect} from 'chai';
+import { describe, it} from 'mocha';
+import { mock, when, instance } from 'ts-mockito';
 import {Query} from "../../../src/data-extraction/github/query";
 import {GithubUserInfo} from "../../../src/data-extraction/github/githubUserInfo";
 
-let fakeToken : string = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9";
+let fakeStorage : any = "fakeDataRetrieved";
 let fakeUsername : string = "fakeUsername";
 let fakeLocation: string = "montreal";
 let fakeEndCusor: string = "Y3Vyc29yOjEwMDA="
 let fakeLastCreatedAt: string = "2018-09-27"
 
-describe('Test getData function', function () {
-    it('should call the github V4 API', function () {
 
-        let fakeQuery : Query = new Query(fakeToken);
+describe.only('GithubUserInfo Queries with Mock', function () {
 
-        //when the get data is called
-        fakeQuery.getData(fakeUsername)
-            .then(() =>
-                //expect the github GraphQL api called
-                expect(fetch("https://api.github.com/graphql"))
-            );
-    });
+it('should call firstQuery function and then the github V4 API', function () {
+    const mockedGithubUser: GithubUserInfo = mock(GithubUserInfo);
+    const trial_Instance : GithubUserInfo = instance(mockedGithubUser);
+    when(mockedGithubUser.firstQuery(fakeLocation)).thenReturn(fakeStorage);
+    let result: Promise<string> = trial_Instance.firstQuery(fakeLocation);
+    expect(result).equals(fakeStorage);
 });
 
-describe('Test firstQuery users location function', function () {
-    it('should call the github V4 API', function () {
-
-        let fakeQuery : GithubUserInfo = new GithubUserInfo();
-
-        //when the first query is called
-        fakeQuery.firstQuery(fakeLocation)
-            .then(() =>
-                //expect the github GraphQL api called
-                expect(fetch("https://api.github.com/graphql"))
-            );
-    });
+it('should call getData function and then the github V4 API', function () {
+    const mockedGithubUser: GithubUserInfo = mock(GithubUserInfo);
+    const trial_Instance : GithubUserInfo = instance(mockedGithubUser);
+    when(mockedGithubUser.getData(fakeLocation,fakeEndCusor)).thenReturn(fakeStorage);
+    let result: Promise<string> = trial_Instance.getData(fakeLocation,fakeEndCusor);
+    expect(result).equals(fakeStorage);
 });
 
-describe('Test getData function with an endCursor', function () {
-    it('should call the github V4 API', function () {
 
-        let fakeQuery : GithubUserInfo = new GithubUserInfo();
-
-        //when the get data is called
-        fakeQuery.getData(fakeLocation,fakeEndCusor)
-            .then(() =>
-                //expect the github GraphQL api called
-                expect(fetch("https://api.github.com/graphql"))
-            );
-    });
+it('should call getDataBefore function and then the github V4 API', function () {
+    const mockedGithubUser: GithubUserInfo = mock(GithubUserInfo);
+    const trial_Instance : GithubUserInfo = instance(mockedGithubUser);
+    when(mockedGithubUser.getDataBefore(fakeLocation,fakeLastCreatedAt)).thenReturn(fakeStorage);
+    let result: Promise<string> = trial_Instance.getDataBefore(fakeLocation,fakeLastCreatedAt);
+    expect(result).equals(fakeStorage);
 });
 
-describe('Test getDataBefore function', function () {
-    it('should call the github V4 API', function () {
-
-        let fakeQuery : GithubUserInfo = new GithubUserInfo();
-
-        //when the get data is called
-        fakeQuery.getDataBefore(fakeLocation,fakeLastCreatedAt)
-            .then(() =>
-                //expect the github GraphQL api called
-                expect(fetch("https://api.github.com/graphql"))
-            );
-    });
+it('should call getDataBeforeWithEndCursor function and then the github V4 API', function () {
+    const mockedGithubUser: GithubUserInfo = mock(GithubUserInfo);
+    const trial_Instance : GithubUserInfo = instance(mockedGithubUser);
+    when(mockedGithubUser.getDataBeforeWithEndCursor(fakeLocation,fakeLastCreatedAt,fakeEndCusor)).thenReturn(fakeStorage);
+    let result: Promise<string> = trial_Instance.getDataBeforeWithEndCursor(fakeLocation,fakeLastCreatedAt,fakeEndCusor);
+    expect(result).equals(fakeStorage);
 });
 
-describe('Test getDataBeforeWithEndCursor function', function () {
-    it('should call the github V4 API', function () {
+});
 
-        let fakeQuery : GithubUserInfo = new GithubUserInfo();
+describe('Query class with Mock',function(){
 
-        //when the get data is called
-        fakeQuery.getDataBeforeWithEndCursor(fakeLocation,fakeLastCreatedAt,fakeEndCusor)
-            .then(() =>
-                //expect the github GraphQL api called
-                expect(fetch("https://api.github.com/graphql"))
-            );
-    });
+it('should call getData function and then the github V4 API',function(){
+    const mockedQueryClass: Query = mock(Query);
+    const trial_Instance : Query = instance(mockedQueryClass);
+    when(mockedQueryClass.getData(fakeUsername)).thenReturn(fakeStorage);
+    let result: Promise<string> = trial_Instance.getData(fakeUsername);
+    expect(result).equals(fakeStorage);
+});
+
 });
