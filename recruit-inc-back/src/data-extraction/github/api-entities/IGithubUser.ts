e300a97fc5bab3960642d6f0535b7221ff38ba40
momo2793 <mohamed_2_27@hotmail.com>
2018-10-07 22:13:05 -0400

    #19 coauthor: @rena-21 @MewtR update interface for Github users to include repositories and languages

--- recruit-inc-back/src/data-extraction/github/api-entities/IGithubUser.ts
+++ recruit-inc-back/src/data-extraction/github/api-entities/IGithubUser.ts
@@ -1,6 +1,12 @@
 export interface IGithubUser{
     login: string
     location?: string,
+    Languages:{
+        name: string
+    },
+    repositories:{
+        name: string
+    },  
     email?: string,
     company?: string,
     isHireable?: boolean,
