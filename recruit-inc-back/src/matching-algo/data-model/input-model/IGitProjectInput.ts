fb87817de44cd39cced82f06a931711c4dafc08f
BenjaminTanguay <benjamin.tanguay@gmail.com>
2018-10-19 14:21:42 -0400

    #46 Added comments to the interface to make things clearer
    Co-authored-by: Winterhart <m.user.work@gmail.com>

--- recruit-inc-back/src/matching-algo/data-model/input-model/IGitProjectInput.ts
+++ recruit-inc-back/src/matching-algo/data-model/input-model/IGitProjectInput.ts
@@ -7,5 +7,7 @@ export interface IGitProjectInput{
     projectName: string
     applicantCommits: ICommit[]
     projectStructure: IProjectStructure[]
+    // Here we want an array of file paths. We basically want to know where the relevant files (package.json, etc)
+    // were downloaded.
     downloadedSourceFile: string[]
 }
