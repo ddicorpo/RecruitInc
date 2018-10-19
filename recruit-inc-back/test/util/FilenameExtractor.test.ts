d3ea9ac1998f1d396d6c9a077ab3104b5efd9ae3
BenjaminTanguay <benjamin.tanguay@gmail.com>
2018-10-19 19:07:59 -0400

    #46 Added a filename extractor to process paths
    Co-authored-by: Winterhart <m.user.work@gmail.com>

---
+++ recruit-inc-back/test/util/FilenameExtractor.test.ts
@@ -0,0 +1,19 @@
+import { expect } from "chai";
+import 'mocha';
+import {FilenameExtractor} from "../../src/util/FilenameExtractor";
+
+describe('Test filename extractor', () => {
+
+    it('Should extract properly a filename with the extension out of a simple path', () => {
+        // GIVEN
+        const path: string = "toto/tata/titi/tutu/filename.extension";
+
+        // WHEN
+        const filename = FilenameExtractor.extract(path);
+        // THEN
+        expect(filename).to.be.equal("filename.extension");
+    });
+
+
+});
+
