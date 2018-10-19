096a2192fab3f9ac182b97c524fda81eb913b992
winterhart <m.user.work@gmail.com>
2018-10-19 16:55:12 -0400

    #46 Coauthor: @BenjaminTanguay creation of fake data for IProjectStructure

--- recruit-inc-back/test/matching-algo/data-model/GitProjectInputExample.ts
+++ recruit-inc-back/test/matching-algo/data-model/GitProjectInputExample.ts
@@ -1,5 +1,6 @@
 import {ICommit} from "../../../src/matching-algo/data-model/input-model/ICommit";
 import {ISingleFileCommit} from "../../../src/matching-algo/data-model/input-model/ISingleFileCommit";
+import { IProjectStructure } from "../../../src/matching-algo/data-model/input-model/IProjectStructure";
 
 var project = {
     "id": 8919310,
@@ -29,216 +30,172 @@ var project = {
     }
 };
 
-var repositoryTree = [
+const repositoryTree: IProjectStructure[] = [
     {
-        "id": "d670f68044212dfb9cd0a3b3dc8b1a31e1c1c78c",
-        "name": ".gitignore",
-        "type": "blob",
-        "path": ".gitignore",
-        "mode": "100644"
+        "fileId": "d670f68044212dfb9cd0a3b3dc8b1a31e1c1c78c",
+        "fileName": ".gitignore",
+        "filePath": ".gitignore",
+
     },
     {
-        "id": "98d3005dfac1214832dfe056918cc65b5619a722",
-        "name": "README.md",
-        "type": "blob",
-        "path": "README.md",
-        "mode": "100644"
+        "fileId": "98d3005dfac1214832dfe056918cc65b5619a722",
+        "fileName": "README.md",
+        "filePath": "README.md",
+
     },
     {
-        "id": "546bc66d98537daf8c4c6b82a6b4b09474f461e3",
-        "name": "manage.py",
-        "type": "blob",
-        "path": "backend/manage.py",
-        "mode": "100755"
+        "fileId": "546bc66d98537daf8c4c6b82a6b4b09474f461e3",
+        "fileName": "manage.py",
+        "filePath": "backend/manage.py",
     },
     {
-        "id": "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
-        "name": "__init__.py",
-        "type": "blob",
-        "path": "backend/src/__init__.py",
-        "mode": "100644"
+        "fileId": "e69de29bb2d1d6434b8b29ae775ad8c2e48c5391",
+        "fileName": "__init__.py",
+        "filePath": "backend/src/__init__.py",
+
     },
     {
-        "id": "2e54c01e987cfe65e65dda62532a28f59896f82b",
-        "name": "settings.py",
-        "type": "blob",
-        "path": "backend/src/settings.py",
-        "mode": "100644"
+        "fileId": "2e54c01e987cfe65e65dda62532a28f59896f82b",
+        "fileName": "settings.py",
+        "filePath": "backend/src/settings.py",
+
     },
     {
-        "id": "4e874c8be80c3372bf8c35d4d41e9ad3aeee17ca",
-        "name": "urls.py",
-        "type": "blob",
-        "path": "backend/src/urls.py",
-        "mode": "100644"
+        "fileId": "4e874c8be80c3372bf8c35d4d41e9ad3aeee17ca",
+        "fileName": "urls.py",
+        "filePath": "backend/src/urls.py",
+
     },
     {
-        "id": "cc4b938800d72983841086b83dd874b501fb05a2",
-        "name": "wsgi.py",
-        "type": "blob",
-        "path": "backend/src/wsgi.py",
-        "mode": "100644"
+        "fileId": "cc4b938800d72983841086b83dd874b501fb05a2",
+        "fileName": "wsgi.py",
+        "filePath": "backend/src/wsgi.py",
+
     },
     {
-        "id": "30a6c7f1b6dae5f390575e490a085d5a8c496429",
-        "name": "env.js",
-        "type": "blob",
-        "path": "frontend/config/env.js",
-        "mode": "100644"
+        "fileId": "30a6c7f1b6dae5f390575e490a085d5a8c496429",
+        "fileName": "env.js",
+        "filePath": "frontend/config/env.js",
+
     },
     {
-        "id": "8f65114812a4e5726d2e4148cd15481c33e1cfec",
-        "name": "cssTransform.js",
-        "type": "blob",
-        "path": "frontend/config/jest/cssTransform.js",
-        "mode": "100644"
+        "fileId": "8f65114812a4e5726d2e4148cd15481c33e1cfec",
+        "fileName": "cssTransform.js",
+        "filePath": "frontend/config/jest/cssTransform.js",
+
     },
     {
-        "id": "9e4047d358ded7f5ed067f1f2fa7c9b178f7ae18",
-        "name": "fileTransform.js",
-        "type": "blob",
-        "path": "frontend/config/jest/fileTransform.js",
-        "mode": "100644"
+        "fileId": "9e4047d358ded7f5ed067f1f2fa7c9b178f7ae18",
+        "fileName": "fileTransform.js",
+        "filePath": "frontend/config/jest/fileTransform.js",
+
     },
     {
-        "id": "6d16efc99eb817885026dcc70e4467a1276ffd53",
-        "name": "paths.js",
-        "type": "blob",
-        "path": "frontend/config/paths.js",
-        "mode": "100644"
+        "fileId": "6d16efc99eb817885026dcc70e4467a1276ffd53",
+        "fileName": "paths.js",
+        "filePath": "frontend/config/paths.js",
+
     },
     {
-        "id": "66dff0a8b1419497fc2be6b9eb3ba88bf37f335c",
-        "name": "polyfills.js",
-        "type": "blob",
-        "path": "frontend/config/polyfills.js",
-        "mode": "100644"
+        "fileId": "66dff0a8b1419497fc2be6b9eb3ba88bf37f335c",
+        "fileName": "polyfills.js",
+        "filePath": "frontend/config/polyfills.js",
+
     },
     {
-        "id": "740a444ceb7290436024228eb7ce58950bbb9868",
-        "name": "webpack.config.dev.js",
-        "type": "blob",
-        "path": "frontend/config/webpack.config.dev.js",
-        "mode": "100644"
+        "fileId": "740a444ceb7290436024228eb7ce58950bbb9868",
+        "fileName": "webpack.config.dev.js",
+        "filePath": "frontend/config/webpack.config.dev.js",
+
     },
     {
-        "id": "5a89a0695e3e8028d20c15a139fc5e92f9dccb98",
-        "name": "webpack.config.prod.js",
-        "type": "blob",
-        "path": "frontend/config/webpack.config.prod.js",
-        "mode": "100644"
+        "fileId": "5a89a0695e3e8028d20c15a139fc5e92f9dccb98",
+        "fileName": "webpack.config.prod.js",
+        "filePath": "frontend/config/webpack.config.prod.js",
+
     },
     {
-        "id": "f12d315944e02672f634389b15f745bc1761d09b",
-        "name": "webpackDevServer.config.js",
-        "type": "blob",
-        "path": "frontend/config/webpackDevServer.config.js",
-        "mode": "100644"
+        "fileId": "f12d315944e02672f634389b15f745bc1761d09b",
+        "fileName": "webpackDevServer.config.js",
+        "filePath": "frontend/config/webpackDevServer.config.js",
+
     },
     {
-        "id": "bfad87deae43185ad258c69cd470b97aff92dacf",
-        "name": "package-lock.json",
-        "type": "blob",
-        "path": "frontend/package-lock.json",
-        "mode": "100644"
+        "fileId": "bfad87deae43185ad258c69cd470b97aff92dacf",
+        "fileName": "package-lock.json",
+        "filePath": "frontend/package-lock.json",
+
     },
     {
-        "id": "328aca6548bec2cbee4086c494e4964769ccf510",
-        "name": "package.json",
-        "type": "blob",
-        "path": "frontend/package.json",
-        "mode": "100644"
+        "fileId": "328aca6548bec2cbee4086c494e4964769ccf510",
+        "fileName": "package.json",
+        "filePath": "frontend/package.json",
     },
     {
-        "id": "a11777cc471a4344702741ab1c8a588998b1311a",
-        "name": "favicon.ico",
-        "type": "blob",
-        "path": "frontend/public/favicon.ico",
-        "mode": "100644"
+        "fileId": "a11777cc471a4344702741ab1c8a588998b1311a",
+        "fileName": "favicon.ico",
+        "filePath": "frontend/public/favicon.ico",
     },
     {
-        "id": "ed0ebafa1b7c3057e5fc3eddbdf9e4c2c46de805",
-        "name": "index.html",
-        "type": "blob",
-        "path": "frontend/public/index.html",
-        "mode": "100644"
+        "fileId": "ed0ebafa1b7c3057e5fc3eddbdf9e4c2c46de805",
+        "fileName": "index.html",
+        "filePath": "frontend/public/index.html",
     },
     {
-        "id": "ef19ec243e739479802a5553d0b38a18ed845307",
-        "name": "manifest.json",
-        "type": "blob",
-        "path": "frontend/public/manifest.json",
-        "mode": "100644"
+        "fileId": "ef19ec243e739479802a5553d0b38a18ed845307",
+        "fileName": "manifest.json",
+        "filePath": "frontend/public/manifest.json",
     },
     {
-        "id": "5709f48cfcbdc0f76509ff9e40e4756f741acaa1",
-        "name": "build.js",
-        "type": "blob",
-        "path": "frontend/scripts/build.js",
-        "mode": "100644"
+        "fileId": "5709f48cfcbdc0f76509ff9e40e4756f741acaa1",
+        "fileName": "build.js",
+        "filePath": "frontend/scripts/build.js",
     },
     {
-        "id": "c5acbe5773106a095ff8550e55368477f74f9f88",
-        "name": "start.js",
-        "type": "blob",
-        "path": "frontend/scripts/start.js",
-        "mode": "100644"
+        "fileId": "c5acbe5773106a095ff8550e55368477f74f9f88",
+        "fileName": "start.js",
+        "filePath": "frontend/scripts/start.js",
     },
     {
-        "id": "45a643ac05d218262f10a951ebc85e1d75fe8929",
-        "name": "test.js",
-        "type": "blob",
-        "path": "frontend/scripts/test.js",
-        "mode": "100644"
+        "fileId": "45a643ac05d218262f10a951ebc85e1d75fe8929",
+        "fileName": "test.js",
+        "filePath": "frontend/scripts/test.js",
     },
     {
-        "id": "c5c6e8a68adcb5249ebdf283ffb34b8531d8b1f0",
-        "name": "App.css",
-        "type": "blob",
-        "path": "frontend/src/App.css",
-        "mode": "100644"
+        "fileId": "c5c6e8a68adcb5249ebdf283ffb34b8531d8b1f0",
+        "fileName": "App.css",
+        "filePath": "frontend/src/App.css",
     },
     {
-        "id": "203067e4d7553b342bf6507a30ced6f5c806e1c5",
-        "name": "App.js",
-        "type": "blob",
-        "path": "frontend/src/App.js",
-        "mode": "100644"
+        "fileId": "203067e4d7553b342bf6507a30ced6f5c806e1c5",
+        "fileName": "App.js",
+        "filePath": "frontend/src/App.js",
     },
     {
-        "id": "a754b201bf9c6caf5271293588189fb4210f99d1",
-        "name": "App.test.js",
-        "type": "blob",
-        "path": "frontend/src/App.test.js",
-        "mode": "100644"
+        "fileId": "a754b201bf9c6caf5271293588189fb4210f99d1",
+        "fileName": "App.test.js",
+        "filePath": "frontend/src/App.test.js",
     },
     {
-        "id": "b4cc7250b98cb3f1a2dd5bec134296c6942344d9",
-        "name": "index.css",
-        "type": "blob",
-        "path": "frontend/src/index.css",
-        "mode": "100644"
+        "fileId": "b4cc7250b98cb3f1a2dd5bec134296c6942344d9",
+        "fileName": "index.css",
+        "filePath": "frontend/src/index.css",
     },
     {
-        "id": "fae3e3500cf003c76f0065ffd12df759c9ccb6aa",
-        "name": "index.js",
-        "type": "blob",
-        "path": "frontend/src/index.js",
-        "mode": "100644"
+        "fileId": "fae3e3500cf003c76f0065ffd12df759c9ccb6aa",
+        "fileName": "index.js",
+        "filePath": "frontend/src/index.js",
     },
     {
-        "id": "6b60c1042f58d9fabb75485aa3624dddcf633b5c",
-        "name": "logo.svg",
-        "type": "blob",
-        "path": "frontend/src/logo.svg",
-        "mode": "100644"
+        "fileId": "6b60c1042f58d9fabb75485aa3624dddcf633b5c",
+        "fileName": "logo.svg",
+        "filePath": "frontend/src/logo.svg",
     },
     {
-        "id": "a3e6c0cfc10de36d6bcb38c52e00c012e985d68e",
-        "name": "registerServiceWorker.js",
-        "type": "blob",
-        "path": "frontend/src/registerServiceWorker.js",
-        "mode": "100644"
+        "fileId": "a3e6c0cfc10de36d6bcb38c52e00c012e985d68e",
+        "fileName": "registerServiceWorker.js",
+        "filePath": "frontend/src/registerServiceWorker.js",
+
     }
 ];
 
