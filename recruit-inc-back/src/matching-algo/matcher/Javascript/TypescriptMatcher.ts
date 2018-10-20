0dc656d600a4580c02e87fc89d2042eeee54ae9e
winterhart <m.user.work@gmail.com>
2018-10-20 17:01:55 -0400

    #46 modification of inherited method for Typescript matcher Co-authored-by: BenjaminTanguay <benjamin.tanguay@gmail.com>

--- recruit-inc-back/src/matching-algo/matcher/Javascript/TypescriptMatcher.ts
+++ recruit-inc-back/src/matching-algo/matcher/Javascript/TypescriptMatcher.ts
@@ -2,8 +2,9 @@ import {AbstractLanguageMatcher} from "../AbstractLanguageMatcher";
 import {IGitProjectInput} from "../../data-model/input-model/IGitProjectInput";
 import {ITargetMatcher} from "../../data-model/matcher-model/ITargetMatcher";
 import {Technologies} from "../../data-model/output-model/Technologies";
+import {AbstractFrameworkMatcher} from "../AbstractFrameworkMatcher";
 
-export class TypescriptMatcher extends AbstractLanguageMatcher {
+export class TypescriptMatcher extends AbstractFrameworkMatcher {
     public constructor(projectsInput: IGitProjectInput, matchingTargets: ITargetMatcher[],
                        targetTechnology : Technologies, matchingExtensions: string[]){
 
