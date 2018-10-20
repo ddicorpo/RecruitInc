b6c780358510419f8237aa0fd698c5e861457da5
BenjaminTanguay <benjamin.tanguay@gmail.com>
2018-10-20 17:07:52 -0400

    #46 Added a way for framework to subscribe to languages for analysis
    Co-authored-by: Winterhart <m.user.work@gmail.com>

--- recruit-inc-back/src/matching-algo/matcher/AbstractLanguageMatcher.ts
+++ recruit-inc-back/src/matching-algo/matcher/AbstractLanguageMatcher.ts
@@ -2,12 +2,19 @@ import {AbstractMatcher} from "./AbstractMatcher";
 import {Technologies} from "../data-model/output-model/Technologies";
 import {IGitProjectInput} from "../data-model/input-model/IGitProjectInput";
 import {ITargetMatcher} from "../data-model/matcher-model/ITargetMatcher";
+import {AbstractFrameworkMatcher} from "./AbstractFrameworkMatcher";
 
 
 export abstract class AbstractLanguageMatcher extends AbstractMatcher {
 
+    private frameworks: AbstractFrameworkMatcher[];
 
-    public constructor(projectsInput: IGitProjectInput, matchingTargets: ITargetMatcher[], targetTechnology : Technologies, matchingExtensions: string[]){
+    public constructor(projectsInput: IGitProjectInput, matchingTargets: ITargetMatcher[], targetTechnology : Technologies, matchingExtensions: string[], frameworks: AbstractFrameworkMatcher[] = []){
         super(projectsInput, matchingTargets, targetTechnology, matchingExtensions);
+        this.frameworks = frameworks;
+    }
+
+    public addFramework(framework: AbstractFrameworkMatcher): void {
+        this.frameworks.push(framework);
     }
 }
