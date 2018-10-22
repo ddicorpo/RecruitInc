51ea6f4c8d60a93642205842a91892e6c8467223
Philippe Beaudry <philippe.beaudry96@gmail.com>
2018-10-22 15:56:01 -0400

    Update recruit-inc-back/src/util/FilepathExtractor.ts
    Co-Authored-By: BenjaminTanguay <benjamin.tanguay@gmail.com>

--- recruit-inc-back/src/util/FilepathExtractor.ts
+++ recruit-inc-back/src/util/FilepathExtractor.ts
@@ -2,6 +2,6 @@ export class FilepathExtractor {
 
     static extract(path: string): string {
         // https://stackoverflow.com/questions/2187256/js-most-optimized-way-to-remove-a-filename-from-a-path-in-a-string
-        return path.split("/").slice(0, -1).join("/")+"/"
+        return path.split("/").slice(0, -1).join("/")+"/";
     }
 }
