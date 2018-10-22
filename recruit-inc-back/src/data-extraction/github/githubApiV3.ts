bcf96491a3d612b4a640500fa6a890798841beb6
momo2793 <mohamed_2_27@hotmail.com>
2018-10-22 17:01:17 -0400

    #64 implement queries to retrieve all commits for a user in a Github Repository with specific files modified

--- recruit-inc-back/src/data-extraction/github/githubApiV3.ts
+++ recruit-inc-back/src/data-extraction/github/githubApiV3.ts
@@ -3,30 +3,25 @@ const logger = require('../../logger.js');
 
 export class GithubApiV3 {
 
+    public queryUserCommits(owner: string, repo: string, sha: string  ) : string{
+        logger.info({class: "githubApiV3", method: "queryUserCommits", action: "Querying github's api", params: {owner, repo, sha}}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+        return fetch(`https://api.github.com/repos/${owner}/${repo}/commits/${sha}`, {
+            method: 'GET',
+            headers: {
 
-    public queryData(accessToken: string, query: string ) : string{
-    
-    logger.info({class: "GithubApiV3", method: "queryData", action: "Querying Github's api", params: {accessToken, query}}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
-
-    return fetch(`https://api.github.com/repos/${owner}/${repo}/${commits}`, {
-        method: 'GET',
-        headers: {
-            'Authorization': `Bearer ${accessToken}`,
-        },
-    }).then(response => response.text())
-        .then(body => {
-            logger.info({class: "GithubApiV3", method: "queryData", action: "Result from Github's api", value: body}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
-            console.log(body);
-            return body;
-        })
-        .catch(error => {
-            logger.error({class: "GithubApiV3", method: "queryData", action: "Error from Github's api", value: error}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
-            return error;
-        });
-
-
-
-
-
+                
+                
+                
+            },
+        }).then(response => response.text())
+            .then(body => {
+                logger.info({class: "githubApiV3", method: "queryData", action: "Result from github's api", value: body}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+                console.log(body);
+                return body;
+            })
+            .catch(error => {
+                logger.error({class: "githubApiV3", method: "queryData", action: "Error from github's api", value: error}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+                return error;
+            });
     }
 }
--- recruit-inc-back/src/data-extraction/github/githubUserCommits.ts
+++ recruit-inc-back/src/data-extraction/github/githubUserCommits.ts
@@ -1,4 +1,5 @@
 import { GithubApiV4} from "./githubApiV4";
+import { GithubApiV3} from "./githubApiV3";
 import {IGithubUser} from "./api-entities/IGithubUser"
 
 export class GithubUserCommits {
@@ -7,6 +8,10 @@ export class GithubUserCommits {
     public constructor(accessToken: string = "37780cb5a0cd8bbedda4c9537ebf348a6e402baf" ) {
       this.accessToken = accessToken;
   }
+
+  async getUserCommits(ownerzz: string, repozzz:string, sha:string): Promise<string> {
+    return await new GithubApiV3().queryUserCommits( ownerzz, repozzz, sha);
+}
 
     async gwt(RepoName: string, OwnerUsername: string, UserEmail: string): Promise<string> {
        
@@ -20,6 +25,11 @@ export class GithubUserCommits {
                       ... on Commit {
                         id
                         history(author : {emails: "${UserEmail}"} first: 100) { 
+
+                          pageInfo{
+                            hasNextPage
+                            endCursor
+                          }
                           edges {
                             
                             node {
@@ -48,7 +58,87 @@ export class GithubUserCommits {
 
         return await new GithubApiV4().queryData(this.accessToken, query);
               
-    }                               
+    }     
+
+    async gwtNext(RepoName: string, OwnerUsername: string, UserEmail: string, endCursor: string): Promise<string> {
+       
+
+      let query : string =
+          `query {
+
+            repository(name: "${RepoName}", owner: "${OwnerUsername}") {
+                ref(qualifiedName: "master") {
+                  target {
+                    ... on Commit {
+                      id
+                      history(author : {emails: "${UserEmail}"} first: 100, after: ${endCursor}) { 
+
+                        pageInfo{
+                          hasNextPage
+                          endCursor
+                        }
+                        edges {
+
+                          
+                          node {
+                        pushedDate
+                            oid
+                            changedFiles
+                            commitUrl  
+                            
+                            treeUrl
+                            commitResourcePath
+                            message
+                            author {
+                              name
+                              email
+                              date
+                            
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+               }
+              }
+            }`;
+
+      return await new GithubApiV4().queryData(this.accessToken, query);
+            
+  }     
+  async getCommits(RepoName: IGithubUser, OwnerUsername: IGithubUser, UserEmail: IGithubUser ): Promise<IGithubUser> {
+
+    let data: string = await this.gwt(RepoName.repositories.name, OwnerUsername.repositories.owner.login, UserEmail.email );
+    let jsonData = JSON.parse(data);
+    let pageInfo = jsonData.data.user.repositories.pageInfo;
+    let hasNextPage = pageInfo.hasNextPage;
+    let endCursor : string = JSON.stringify(pageInfo.endCursor);
+    RepoName.repositories = jsonData.data.RepoName.repositories.nodes;
+    OwnerUsername.repositories = jsonData.data.OwnerUsername.repositories.nodes;
+    UserEmail.repositories = jsonData.data.UserEmail.repositories.nodes;
+
+        
+    while(hasNextPage){
+      let nextData : string = await this.gwtNext(RepoName.repositories.name,OwnerUsername.repositories.owner.login,UserEmail.email, endCursor);
+      jsonData = JSON.parse(nextData);
+      pageInfo = jsonData.data.user.repositories.pageInfo;
+      endCursor = JSON.stringify(pageInfo.endCursor);
+      hasNextPage = pageInfo.hasNextPage;
+
+      RepoName.repositories += jsonData.data.RepoName.repositories.nodes;
+    OwnerUsername.repositories += jsonData.data.OwnerUsername.repositories.nodes;
+    UserEmail.repositories += jsonData.data.UserEmail.repositories.nodes;
+      
+      data+=nextData;
+    }
+
+
+    return RepoName;
+    
+
+
+}
 
    
 }
