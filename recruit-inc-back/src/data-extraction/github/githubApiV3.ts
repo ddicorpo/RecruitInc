b1598a41349498e6948276d55cab21e596b4451e
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-10-23 21:54:02 -0400

    #64 Implement functions to download specific files from a github repository as well as routes to test the functionality.

--- recruit-inc-back/src/data-extraction/github/githubApiV3.ts
+++ recruit-inc-back/src/data-extraction/github/githubApiV3.ts
@@ -40,4 +40,23 @@ export class GithubApiV3 {
                 return error;
             });
     }
+
+    public downloadFile(accessToken: string, owner: string, repo: string, path: string  ) : string{
+        logger.info({class: "githubApiV3", method: "downloadFile", action: "Querying github's api", params: {owner, repo, path}}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+        return fetch(`https://api.github.com/repos/${owner}/${repo}/contents/${path}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `token ${accessToken}`,
+            },
+        }).then(response => response.text())
+            .then(body => {
+                logger.info({class: "githubApiV3", method: "downloadFile", action: "Result from github's api", value: body}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+                console.log(body);
+                return body;
+            })
+            .catch(error => {
+                logger.error({class: "githubApiV3", method: "downloadFile", action: "Error from github's api", value: error}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+                return error;
+            });
+    }
 }
