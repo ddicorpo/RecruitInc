692aed36a3572cad23bf8b935ec3bea5801ce8eb
aghiles <aghiles_1@hotmail.com>
2018-10-25 21:01:42 -0400

    #65 added another test for user query class

--- recruit-inc-back/test/data-extraction/gitlab/queries/UserQuery.test.ts
+++ recruit-inc-back/test/data-extraction/gitlab/queries/UserQuery.test.ts
@@ -3,7 +3,19 @@ import {GitlabQueryExecutor} from "../../../../src/data-extraction/gitlab/query-
 import {UserQuery} from "../../../../src/data-extraction/gitlab/queries/UserQuery";
 import {IGitlabUser} from "../../../../src/data-extraction/gitlab/api-entities/IGitlabUser";
 
-describe('User query class', function () {
+
+const nock = require('nock');
+
+describe.only('User query class', function () {
+    beforeEach(() => {
+        //mocking the API
+          nock('https://api.gitlab.com')
+          .post('/api/v4')
+          .reply(200,{
+            gitlabUserPromise: [{ id: 212577 }],
+          });
+      });
+    
     it('should return the correct query after building it', function () {
         let username: string = "Hello";
         let gitlabUserQueryExecutor = new GitlabQueryExecutor<IGitlabUser[]>();
@@ -14,4 +26,20 @@ describe('User query class', function () {
         let actual: string =  "https://gitlab.com/api/v4/users?username=Hello";
         expect(expected).to.equal(actual);
     });
+
+    
+  
+      it('should return a user with those specifications--> id ,name ,username, state ,avatar_url, web_url ', () => {
+        let username: string = "rosarior"
+        let gitlabUserQueryExecutor = new GitlabQueryExecutor<IGitlabUser[]>();
+        let userQuery: UserQuery = new UserQuery(username, gitlabUserQueryExecutor);
+        userQuery.buildQuery(); 
+        
+        return userQuery.executeQuery()
+        .then(response => response[0].id)
+        .then(response => expect(response).to.equal(212577));
+
+      });
+
+
 });
