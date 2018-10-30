4ccf02c9a5690499844a195d1bc5e129858f5113
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-10-29 21:37:18 -0400

    #64 Write to verify that files are properly dowloaded and that their contents are converted decoded so that they appear as plain text.

--- recruit-inc-back/test/data-extraction/github/GithubDownloadedFilesPath.test.ts
+++ recruit-inc-back/test/data-extraction/github/GithubDownloadedFilesPath.test.ts
@@ -5,19 +5,41 @@ import {IGithubUser} from "../../../src/data-extraction/github/api-entities/IGit
 
 
 const fs = require('fs');
+const nock = require('nock');
+const downloadedFileResponse = require('./downloadedFileResponse.js');
 
-  describe.only('Write to file', () => {
+  describe('Write to file', () => {
 
     it('should write the contents of a downloaded file locally', function() {
-
+ 
+        fs.closeSync(fs.openSync("test/data-extraction/github/writeToFileTest.txt", 'w'));
         let test = new GithubDownloadedFilesPath();
         test.writeToFile("This is a test", "test/data-extraction/github/writeToFileTest.txt" );
-        let exists : boolean = fs.existsSync("./test/data-extraction/github/writeToFileTest.txt");
-        expect(exists).to.be.true;
-        fs.unlinkSync("test/data-extraction/github/writeToFileTest.txt");
-        exists = fs.existsSync("./test/data-extraction/github/writeToFileTest.txt");
-        expect(exists).to.be.false;
+        let exists : boolean = fs.existsSync("test/data-extraction/github/writeToFileTest.txt");
 
+        expect(exists).to.be.true;
+
+        fs.unlinkSync("test/data-extraction/github/writeToFileTest.txt");
+
+        let exists2 : boolean = fs.existsSync("test/data-extraction/github/writeToFileTest.txt");
+        expect(exists2).to.be.false;
     });
   });
 
+  describe('Retrieve the contents of a file', () => {
+
+    it('should download a file', async function() {
+
+        nock('https://api.github.com')
+        .get('/repos/AyoubeAkaouch/MinistocksRework/contents/src/main/AndroidManifest.xml')
+        .reply(200, downloadedFileResponse)
+
+        let test = new GithubDownloadedFilesPath();
+        let result : {name: string, path: string, content: string} = await test.downloadFile('AyoubeAkaouch', 'MinistocksRework', 'src/main/AndroidManifest.xml');
+        expect(result.name).to.equal('AndroidManifest.xml');
+        expect(result.path).to.equal('src/main/AndroidManifest.xml');
+        let contents : string = fs.readFileSync('test/data-extraction/github/AndroidManifest.xml', 'utf8');
+        expect(result.content).to.equal(contents);
+
+    });
+  });
