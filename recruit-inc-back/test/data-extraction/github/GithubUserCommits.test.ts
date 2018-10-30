e4d3e1c7e7ba5587370f3c12cbea91ce484d3ed8
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-10-29 21:57:24 -0400

    #64 Write test to verify that the files affected by a commit can be properly extracted.

--- recruit-inc-back/test/data-extraction/github/GithubUserCommits.test.ts
+++ recruit-inc-back/test/data-extraction/github/GithubUserCommits.test.ts
@@ -9,6 +9,7 @@ let test : GithubUserCommits = new GithubUserCommits();
 //let user : IGithubUser = {login: "MewtR", url: "", createdAt: ""};
 
   const GithubUserCommitsRetrieval = require('./GithubUserCommits.js');
+  const FilesAffectedByCommit = require('./filesAffectedByCommit.js');
 
   describe('Get commits from a user in a github repo', () => {
     beforeEach(() => {
@@ -28,5 +29,26 @@ let test : GithubUserCommits = new GithubUserCommits();
         console.log(response);
         expect(response["node"]["oid"]).to.equal("eff56ed52a77dfb8020a7ab11468138c00a3284f");
         });
+    });
+  });
+
+  describe('Get files affected by a given commit', () => {
+
+    it('should return the file path as well as the lines added and deleted of the files affected by a commit', async function() {
+
+        nock('https://api.github.com')
+        .get('/repos/AyoubeAkaouch/MinistocksRework/commits/950e3b390cc8f569183cfbb5ec924537b083c191')
+        .reply(200, FilesAffectedByCommit)
+
+        let test = new GithubUserCommits();
+        let result : {filePath: string, lineAdded: number, lineDeleted: number}[] = await test.getFilesAffectedByCommit('AyoubeAkaouch', 'MinistocksRework', '950e3b390cc8f569183cfbb5ec924537b083c191');
+
+        //This commit affected 8 files
+        expect(result.length).to.equal(8);
+
+        //Check a specific file
+        expect(result[7].filePath).to.equal('src/test/java/nitezh/ministock/mocks/MockWidget.java');
+        expect(result[7].lineAdded).to.equal(5);
+        expect(result[7].lineDeleted).to.equal(0);
     });
   });
