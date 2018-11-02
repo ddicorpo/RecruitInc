e9af0761196b46708268c65f3cd505409eff03fb
philbeaudry <philippe.beaudry96@gmail.com>
2018-11-01 23:26:27 -0400

    #41 Changed the pipeline of the code and token retrieval to be streamed lined

--- recruit-inc-front/pages/profile.tsx
+++ recruit-inc-front/pages/profile.tsx
@@ -4,18 +4,42 @@ import Header from '../components/Header'
 class App extends React.Component {
 
     state = {
-        username: ''
+        username: '',
+        data: ''
     };
 
     constructor(props: any) {
         super(props);
         this.state = {
-            username: ''
+            username: '',
+            data: ''
         }
     }
 
     handleChange(event: any) {
         this.setState({username: event.target.value});
+    }
+
+    componentDidMount() {
+        let url: string = window.location.href;
+
+        let platform :string = url.substring(url.indexOf("platform=") + 9,  url.indexOf("&"));
+        let code: string = url.substring(url.indexOf("code=") + 5);
+
+        console.log(platform);
+        if(code != null) {
+            return fetch(`http://localhost:6969/api/oauth/oauthcode/${platform}/${code}`)
+                .then((response) => {
+                    return Promise.resolve(response.text())
+                })
+                .then(data => {
+                    this.setState({data});
+                    console.log("state", data);
+                });
+        }
+
+        console.log("code: " + code);
+        return;
     }
 
     render(){
@@ -35,9 +59,10 @@ class App extends React.Component {
                     Github
                 </a>
                 <br/>
-                <a style={{display: "table-cell"}} href={"https://gitlab.com/oauth/authorize?client_id=cf78ad0e83e8c8f5e4cc8b60ef0250e1d1a299cd9f3ec91ec9d54399eb52e102&redirect_uri=http://localhost:3000/OAuth/gitlab&response_type=code"}>
+                <a style={{display: "table-cell"}} href={"https://gitlab.com/oauth/authorize?client_id=cf78ad0e83e8c8f5e4cc8b60ef0250e1d1a299cd9f3ec91ec9d54399eb52e102&redirect_uri=http://localhost:3000/profile?platform=gitlab&response_type=code"}>
                     Gitlab
                 </a>
+                <p>{JSON.stringify(this.state.data)}</p>
             </div>
         );
     }
