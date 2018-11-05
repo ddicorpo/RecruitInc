9e4c729f585dc51d748bd2a13d2562df6b6c0913
winterhart <m.user.work@gmail.com>
2018-11-05 17:32:31 -0500

    #70, #71 Addition of non-passing test (tdd) , making the python matcher works

---
+++ recruit-inc-back/test/matching-algo/matcher/DjangoMatcher.test.ts
@@ -0,0 +1,44 @@
+import { expect } from "chai";
+import 'mocha';
+import {dataEntry} from "../data-model/GitProjectInputExample"
+import { projectOutput } from "../data-model/GitProjectOutputExample"
+import {MatcherClient} from "../../../src/matching-algo/matcher-client/MatcherClient";
+import {AbstractLanguageMatcher} from "../../../src/matching-algo/matcher/AbstractLanguageMatcher";
+import {AbstractFrameworkMatcher} from "../../../src/matching-algo/matcher/AbstractFrameworkMatcher";
+import {IGitProjectSummary} from "../../../src/matching-algo/data-model/output-model/IGitProjectSummary";
+import {IGitProjectOutput} from "../../../src/matching-algo/data-model/output-model/IGitProjectOutput";
+import {DjangoMatcher} from "../../../src/matching-algo/matcher/Python/DjangoMatcher";
+import {PythonMatcher} from "../../../src/matching-algo/matcher/Python/PythonMatcher";
+describe('Test react matching algorithm', () => {
+
+    it('Should return the correct output object', () => {
+        // GIVEN
+        // Setup Language Matcher with Python and django
+        const djangoMatcher: AbstractFrameworkMatcher = new DjangoMatcher();
+        const pythonMatcher: AbstractLanguageMatcher = new PythonMatcher();
+
+        pythonMatcher.addFramework(djangoMatcher);
+
+        const customLanguageMatchers: AbstractLanguageMatcher[] = [
+            pythonMatcher
+        ];
+        const client: MatcherClient = new MatcherClient(dataEntry, customLanguageMatchers);
+
+        // WHEN
+        const computedOutput: IGitProjectSummary = client.execute();
+
+        // THEN
+        let index: number = 0;
+        for(const proj in computedOutput.projectsOutput){
+            const projectSum : IGitProjectOutput = computedOutput.projectsOutput[index];
+            const computedProjectSum : IGitProjectOutput = projectOutput.projectsOutput[index];
+            index++;
+            if(JSON.stringify(projectSum) !== JSON.stringify(computedProjectSum)){
+                console.log("expected: " + JSON.stringify(projectSum));
+                console.log("actural: " + JSON.stringify(computedProjectSum));
+                expect.fail();
+            }
+        }
+    });
+
+});
