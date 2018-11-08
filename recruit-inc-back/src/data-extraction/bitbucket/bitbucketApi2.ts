8bb3953cb569957acf0a03b2c4dfe0daed7d310f
23jams <23jamesff@gmail.com>
2018-11-08 00:59:07 -0500

    #66 WIP, fixed array formatting errors for typescript.

--- recruit-inc-back/src/data-extraction/bitbucket/bitbucketApi2.ts
+++ recruit-inc-back/src/data-extraction/bitbucket/bitbucketApi2.ts
@@ -33,7 +33,7 @@ export class BitbucketApi2 {
 
                 let iterator:number  = 0;
 
-                let allGitProjectInput: IGitProjectInput[];
+                let allGitProjectInput: Array<any> = new Array<any>();;
 
                 while (iterator < body.values.length) {
                     fs.writeFile('bitbucketRepoList.json', body.values[iterator].slug, function (err) {
@@ -43,20 +43,27 @@ export class BitbucketApi2 {
                         }
                     });
 
-                    this.queryProjectStructInfo(accessToken, user, body.values[iterator].slug)
+                    //this.queryProjectStructInfo(accessToken, user, body.values[iterator].slug)
 
-                    //
-                    // let gitProjectInput: IGitProjectInput = {
-                    //     projectName: body.values[iterator].slug,
-                    //     applicantCommits: this.queryCommitInfo(accessToken, user, body.values[iterator].slug),
-                    //     projectStructure: this.queryProjectStructInfo(accessToken, user, body.values[iterator].slug),
-                    //     downloadedSourceFile: this.querySourceFileInfo(accessToken, user, body.values[iterator].slug)
-                    // };
-                    //
-                    // allGitProjectInput.push(gitProjectInput);
+
+                    let gitProjectInput: IGitProjectInput = new class implements IGitProjectInput {
+                        applicantCommits: ICommit[];
+                        downloadedSourceFile: ISourceFiles[];
+                        projectName: string;
+                        projectStructure: IProjectStructure[];
+                    }
+                    gitProjectInput.projectName = body.values[iterator].slug;
+                    gitProjectInput.applicantCommits = this.queryCommitInfo(accessToken, user, body.values[iterator].slug);
+                    gitProjectInput.projectStructure = this.queryProjectStructInfo(accessToken, user, body.values[iterator].slug);
+                    gitProjectInput.downloadedSourceFile = this.querySourceFileInfo(accessToken, user, body.values[iterator].slug);
+
+
+                    allGitProjectInput.push(gitProjectInput);
 
                     iterator++;
                 }
+
+                console.log("\n\n\n success");
 
                 return body;
             })
@@ -71,7 +78,7 @@ export class BitbucketApi2 {
             });
     }
 
-    public queryCommitInfo(accessToken: string, user: string, repoName: string): ICommit[] {
+    public queryCommitInfo(accessToken: string, user: string, repoName: string): Array<any> {
         logger.info({
             class: "bitbucketApi2",
             method: "queryData ",
@@ -94,18 +101,22 @@ export class BitbucketApi2 {
                 }, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
 
                 let iterator = 0;
-                let allCommits: ICommit[];
+                let allCommits: Array<any> = new Array<any>();
 
                 while (iterator < body.values.length) {
 
                     if (body.values[iterator].author.user != undefined) {
                         if (JSON.stringify(body.values[iterator].author.user.username).match(user)) {
                             let allSingleCommits = this.queryDiffStats(accessToken, user, repoName, body.values[iterator].hash);
-                            let commit: ICommit = {
-                                id: body.values[iterator].hash,
-                                numberOfFileAffected: allSingleCommits.length,
-                                files: allSingleCommits
-                            };
+                            let commit: ICommit = new class implements ICommit {
+                                files: ISingleFileCommit[];
+                                id: string;
+                                numberOfFileAffected: number;
+                            }
+                            commit.id = body.values[iterator].hash;
+                            commit.numberOfFileAffected = allSingleCommits.length;
+                            commit.files = allSingleCommits;
+
                             allCommits.push(commit);
                         }
                     }
@@ -124,7 +135,7 @@ export class BitbucketApi2 {
             });
     }
 
-    public queryDiffStats(accessToken: string, user: string, repoName: string, hash: string): ISingleFileCommit[] {
+    public queryDiffStats(accessToken: string, user: string, repoName: string, hash: string): Array<any> {
         logger.info({
             class: "bitbucketApi2",
             method: "queryData ",
@@ -147,15 +158,19 @@ export class BitbucketApi2 {
                 }, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
 
                 let singleCommitIndex: number = 0;
-                let allSingleCommits: ISingleFileCommit[];
+                let allSingleCommits: Array<any> = new Array<any>();
 
                 while (singleCommitIndex < body.values.length) {
 
-                    let singleFileCommit: ISingleFileCommit = {
-                        filePath: body.values[singleCommitIndex].new.links.self.href,
-                        lineAdded: body.values[singleCommitIndex].lines_added,
-                        lineDeleted: body.values[singleCommitIndex].lines_removed
-                    };
+                    let singleFileCommit: ISingleFileCommit = new class implements ISingleFileCommit {
+                        filePath: string;
+                        lineAdded: number;
+                        lineDeleted: number;
+                    }
+                    singleFileCommit.filePath = body.values[singleCommitIndex].new.links.self.href;
+                    singleFileCommit.lineAdded = body.values[singleCommitIndex].lines_added;
+                    singleFileCommit.lineDeleted = body.values[singleCommitIndex].lines_removed;
+
                     allSingleCommits.push(singleFileCommit);
 
                     singleCommitIndex++;
@@ -174,7 +189,7 @@ export class BitbucketApi2 {
             });
     }
 
-    public querySourceFileInfo(accessToken: string, user: string, repoName: string): ISourceFiles[] {
+    public querySourceFileInfo(accessToken: string, user: string, repoName: string): Array<any> {
         logger.info({
             class: "bitbucketApi2",
             method: "queryData",
@@ -197,17 +212,22 @@ export class BitbucketApi2 {
 
                 let fileIterator: number = 0;
 
-                let allSourceFiles: ISourceFiles[];
+                let allSourceFiles: Array<any> = new Array<any>();
 
                 while (fileIterator < body.values.length) {
                     if (body.values[fileIterator].path = ".gitignore") {
-                        let sourceFile: ISourceFiles = {
-                            filename: body.values[fileIterator].path,
-                            repoFilePath: body.values[fileIterator].links.self.href,
-                            // TODO: Figure out how to download the raw file
+                        let sourceFile: ISourceFiles =  new class implements ISourceFiles {
+                            filename: string;
+                            localFilePath: string;
+                            repoFilePath: string;
+                        }
 
-                            localFilePath: "https://bitbucket.org/" + user + "/" + repoName + "/get/master.zip"
-                        };
+                        sourceFile.filename = body.values[fileIterator].path;
+                        sourceFile.repoFilePath = body.values[fileIterator].links.self.href;
+                        // TODO: Figure out how to download the raw file
+                        //"https://bitbucket.org/" + user + "/" + repoName + "/get/master.zip"
+                        sourceFile.localFilePath = "https://bitbucket.org/" + user + "/" + repoName + "/get/master.zip";
+
 
                         allSourceFiles.push(sourceFile);
                         break;
@@ -355,7 +375,7 @@ export class BitbucketApi2 {
                         innerIterator = 0;
                         tempProjectStructure = [];
                     }
-                    //TODO: FIX THIS, PROBLEM LIKELY IN THE IF BELOW
+                    
                     if (body.values[fileIterator].type === ("commit_file")){
 
                         let projStruct: IProjectStructure = new class implements IProjectStructure {
