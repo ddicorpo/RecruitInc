eebd1c133a17d2faf816f7061c829b7e665fe546
aghiles <aghiles_1@hotmail.com>
2018-11-09 00:16:15 -0500

    #65 coauthor:MewtR changed the tests

--- recruit-inc-back/src/data-extraction/gitlab/query-executor/GitlabQueryExecutor.ts
+++ recruit-inc-back/src/data-extraction/gitlab/query-executor/GitlabQueryExecutor.ts
@@ -3,13 +3,35 @@ import { IGitlabQueryExecutor } from './IGitlabQueryExecutor';
 
 var logger = require('../../../logger.js');
 
+export class GitlabQueryExecutor<Response>
+  implements IGitlabQueryExecutor<Response> {
+  private baseGitlabApi = 'https://gitlab.com/api/v4/';
 
-export class GitlabQueryExecutor<Response> implements IGitlabQueryExecutor<Response> {
-    private baseGitlabApi = "https://gitlab.com/api/v4/";
-    
-    public async executeQuery(query: string) : Promise<Response>{
+  public async executeQuery(query: string): Promise<Response> {
+    logger.info(
+      {
+        class: 'GitlabQueryExecutor',
+        method: 'executeQuery',
+        action: "Querying gitlab's api",
+        params: { query },
+      },
+      { timestamp: new Date().toLocaleTimeString(), processID: process.pid }
+    );
+    return await fetch(query, {
+      method: 'GET',
+    })
+      .then(response => response.text())
+      .then(body => {
+        logger.info(
+          {
+            class: 'GitlabQueryExecutor',
+            method: 'executeQuery',
+            action: "Result from gitlab's api",
+            value: body,
+          },
+          { timestamp: new Date().toLocaleTimeString(), processID: process.pid }
+        );
 
-        console.log(body);
         return JSON.parse(body);
       })
       .catch(error => {
@@ -23,40 +45,53 @@ export class GitlabQueryExecutor<Response> implements IGitlabQueryExecutor<Respo
           { timestamp: new Date().toLocaleTimeString(), processID: process.pid }
         );
 
+        return error;
+      });
+  }
 
-                
-                return JSON.parse(body);
-            })
-            .catch(error => {
-                logger.error({class: "GitlabQueryExecutor", method: "executeQuery", action: "Error from gitlab's api", value: error}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
+  public async executeDownloadQuery(query: string): Promise<Response> {
+    logger.info(
+      {
+        class: 'GitlabQueryExecutor',
+        method: 'executeQuery',
+        action: "Querying gitlab's api",
+        params: { query },
+      },
+      { timestamp: new Date().toLocaleTimeString(), processID: process.pid }
+    );
+    return await fetch(query, {
+      method: 'GET',
+    })
+      .then(response => response.text())
+      .then(body => {
+        logger.info(
+          {
+            class: 'GitlabQueryExecutor',
+            method: 'executeQuery',
+            action: "Result from gitlab's api",
+            value: body,
+          },
+          { timestamp: new Date().toLocaleTimeString(), processID: process.pid }
+        );
 
-                
-                return error;
-            });
-    }
+        return { content: body };
+      })
+      .catch(error => {
+        logger.error(
+          {
+            class: 'GitlabQueryExecutor',
+            method: 'executeQuery',
+            action: "Error from gitlab's api",
+            value: error,
+          },
+          { timestamp: new Date().toLocaleTimeString(), processID: process.pid }
+        );
 
-    public async executeDownloadQuery(query: string) : Promise<Response>{
+        return error;
+      });
+  }
 
-        logger.info({class: "GitlabQueryExecutor", method: "executeQuery", action: "Querying gitlab's api", params: {query}}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
-        return await fetch(query, {
-            method: 'GET',
-        }).then(response => response.text())
-            .then(body => {
-                logger.info({class: "GitlabQueryExecutor", method: "executeQuery", action: "Result from gitlab's api", value: body}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
-
-                
-                return {content: body}
-            })
-            .catch(error => {
-                logger.error({class: "GitlabQueryExecutor", method: "executeQuery", action: "Error from gitlab's api", value: error}, {timestamp: (new Date()).toLocaleTimeString(), processID: process.pid});
-
-                
-                return error;
-            });
-    }
-
-    public getBaseGitlabApi(): string {
-        return this.baseGitlabApi;
-    };
-
+  public getBaseGitlabApi(): string {
+    return this.baseGitlabApi;
+  }
 }
--- recruit-inc-back/test/data-extraction/gitlab/queries/RepositoryTreeQuery.test.ts
+++ recruit-inc-back/test/data-extraction/gitlab/queries/RepositoryTreeQuery.test.ts
@@ -3,18 +3,22 @@ import { GitlabQueryExecutor } from '../../../../src/data-extraction/gitlab/quer
 import { RepositoryTreeQuery } from '../../../../src/data-extraction/gitlab/queries/RepositoryTreeQuery';
 import { IGitlabRepositoryTree } from '../../../../src/data-extraction/gitlab/api-entities/IGitlabRepositoryTree';
 
-
-describe('Repository tree query class', function () {
-    it('should return the correct query after building it', function () {
-        let projectId: number = 1234;
-        let gitlabRepositoryTreeQueryExecutor = new GitlabQueryExecutor<IGitlabRepositoryTree[]>();
-        let repositoryTree: RepositoryTreeQuery = new RepositoryTreeQuery(projectId, gitlabRepositoryTreeQueryExecutor);
-        //pass a value of 1 to take only the first page
-        repositoryTree.buildQuery(1);
-        let expected: string = repositoryTree.getQuery();
-        let actual: string =  "https://gitlab.com/api/v4/projects/1234/repository/tree?recursive=true&per_page=100&page=1";
-        expect(expected).to.equal(actual);
-
-    });
+describe('Repository tree query class', function() {
+  it('should return the correct query after building it', function() {
+    let projectId: number = 1234;
+    let gitlabRepositoryTreeQueryExecutor = new GitlabQueryExecutor<
+      IGitlabRepositoryTree[]
+    >();
+    let repositoryTree: RepositoryTreeQuery = new RepositoryTreeQuery(
+      projectId,
+      gitlabRepositoryTreeQueryExecutor
+    );
+    //pass a value of 1 to take only the first page and a token
+    const accessToken: string = 'durhdwhfg456s4d35w';
+    repositoryTree.buildQuery(1, accessToken);
+    let expected: string = repositoryTree.getQuery();
+    let actual: string =
+      'https://gitlab.com/api/v4/projects/1234/repository/tree?recursive=true&per_page=100&page=1&private_token=durhdwhfg456s4d35w';
+    expect(expected).to.equal(actual);
+  });
 });
-
