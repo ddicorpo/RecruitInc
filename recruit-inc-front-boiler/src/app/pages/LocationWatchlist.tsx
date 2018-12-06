975f24fe586bc8332e8d513deb83df6d8ca3c5b9
philbeaudry <philippe.beaudry96@gmail.com>
2018-12-05 21:15:45 -0500

    #62 Simple template start for the location watchlist

--- recruit-inc-front-boiler/src/app/pages/LocationWatchlist.tsx
+++ recruit-inc-front-boiler/src/app/pages/LocationWatchlist.tsx
@@ -9,8 +9,38 @@ class LocationWatchList extends React.Component<any, any> {
 
     render() {
         return (
-            <div>
-                Hey, this is a location watchlist!!
+            <div className="main-content">
+                <div className="container-fluid">
+                    <div className="page-header">
+                        <h2 className="header-title">Location Watchlist</h2>
+                    </div>
+                    <div className="card">
+                        <div className="card-header border bottom">
+                            <h4 className="card-title">Search and Add</h4>
+                        </div>
+                        <div className="card-body">
+                            <div className="row m-t-30">
+                                <div className="col-md-4">
+                                    <div className="p-h-10">
+                                        <div className="form-group">
+                                            <label className="control-label">Add city to watchlist</label>
+                                            <input type="text" className="form-control" placeholder="City"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="p-h-10">
+                                        <div className="form-group">
+                                            <button id="save" className="btn btn-success form-control"
+                                                    type="button">Add
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
