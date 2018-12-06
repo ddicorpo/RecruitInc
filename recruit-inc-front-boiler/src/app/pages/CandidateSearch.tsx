70d88e977d8562bfb24076cfa55e049dc57ed4d4
philbeaudry <philippe.beaudry96@gmail.com>
2018-12-05 20:44:20 -0500

    #62 First template for the candidate Search page

--- recruit-inc-front-boiler/src/app/pages/CandidateSearch.tsx
+++ recruit-inc-front-boiler/src/app/pages/CandidateSearch.tsx
@@ -1,16 +1,52 @@
 import * as React from 'react';
 
-
 class CandidateSearch extends React.Component<any, any> {
     constructor(props: any) {
         super(props);
     }
 
-
     render() {
         return (
-            <div>
-                Hey, this is a candidate search!!
+            <div className="main-content">
+                <div className="container-fluid">
+                    <div className="page-header">
+                        <h2 className="header-title">Candidate Search</h2>
+                    </div>
+                    <div className="card">
+                        <div className="card-header border bottom">
+                            <h4 className="card-title">Search Filters</h4>
+                        </div>
+                        <div className="card-body">
+                            <div className="row m-t-30">
+                                <div className="col-md-4">
+                                    <div className="p-h-10">
+                                        <div className="form-group">
+                                            <label className="control-label">Language Search</label>
+                                            <input type="text" className="form-control" placeholder="Language"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="p-h-10">
+                                        <div className="form-group">
+                                            <label className="control-label">City Search</label>
+                                            <input type="text" className="form-control" placeholder="City"/>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="col-md-4">
+                                    <div className="p-h-10">
+                                        <div className="form-group">
+                                            <button id="save" className="btn btn-success form-control"
+                                                    type="button">Search
+                                            </button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
