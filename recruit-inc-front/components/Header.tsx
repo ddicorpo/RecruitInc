cfd7840522f91e4a07036dc73b1a04e58ba83021
BenjaminTanguay <benjamin.tanguay@gmail.com>
2018-12-09 15:06:12 -0500

    #62 Readded the header component removed by mistake from the front-end demo folder

---
+++ recruit-inc-front/components/Header.tsx
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import * as React from 'react';
+
+const linkStyle = {
+    marginRight: 15,
+};
+
+const Header = () => (
+    <div>
+        <Link href="/">
+            <a style={linkStyle}>Home</a>
+        </Link>
+        <Link href={'/profile'}>
+            <a style={linkStyle}>Profile</a>
+        </Link>
+        <Link href={'/demo'}>
+            <a style={linkStyle}>Demo</a>
+        </Link>
+        <Link href={'/about'}>
+            <a style={linkStyle}>About</a>
+        </Link>
+        <Link href={'/admin'}>
+            <a style={linkStyle}>Admin</a>
+        </Link>
+        <Link href={'/hr'}>
+            <a style={linkStyle}>hr</a>
+        </Link>
+    </div>
+);
+
+export default Header;
