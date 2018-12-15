3ea83793ba11b785e5b4a7cf06c350eb8255e5df
winterhart <m.user.work@gmail.com>
2018-12-15 15:33:04 -0500

    #21 renaming test directory to match convention, addition of schema user

--- recruit-inc-back/src/data-source/schema/User.ts
+++ recruit-inc-back/src/data-source/schema/User.ts
@@ -1,6 +1,21 @@
 import { prop, Typegoose } from 'typegoose';
 
 export class User extends Typegoose {
-  @prop()
-  username?: string;
+  @prop({ index: true })
+  userId: number;
+
+  @prop({ required: true, unique: true, minlength: 3, maxlength: 150 })
+  username: string;
+
+  @prop({ required: true, minlength: 2, maxlength: 150 })
+  firstName: string;
+
+  @prop({ required: true, minlength: 2, maxlength: 150 })
+  lastName: string;
+  // Hashed Password, don't store unhashed
+  @prop({ required: true })
+  hashedPassword: string;
+
+  @prop({ required: true, unique: true, minlength: 5, maxlength: 300 })
+  email: string;
 }
