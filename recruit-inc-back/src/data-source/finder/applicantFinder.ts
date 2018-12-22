308fd41d802c2f4cba287f93d79dff5e005d77cc
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-12-22 17:58:48 -0500

    #21 Complete the TDG, Finder, schema and model for the applicant table.

--- recruit-inc-back/src/data-source/finder/applicantFinder.ts
+++ recruit-inc-back/src/data-source/finder/applicantFinder.ts
@@ -1,1 +1,58 @@
-//TODO
+import { BaseFinder } from './baseFinder';
+import { ApplicantModel } from '../schema/applicantSchema';
+import { IApplicantModel } from '../../domain/model/IApplicantModel';
+import { UserType } from '../../domain/model/IApplicantModel';
+import { Types } from 'mongoose';
+
+export class ApplicantFinder {
+  private baseFinder: BaseFinder;
+
+  constructor() {
+    this.baseFinder = new BaseFinder(ApplicantModel);
+  }
+
+  public findById(_id: string): Promise<IApplicantModel> {
+    return new Promise((resolve: any, reject: any) => {
+      ApplicantModel.findOne(Types.ObjectId(_id), (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findByPlatformUsername(platformUsername: string): Promise<IApplicantModel> {
+    return new Promise((resolve: any, reject: any) => {
+      ApplicantModel.findOne({ platformUsername: platformUsername }, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findByPlatformEmail(platformEmail: string): Promise<IApplicantModel> {
+    return new Promise((resolve: any, reject: any) => {
+      ApplicantModel.findOne({ platformEmail: platformEmail }, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findByUserType(userType: UserType): Promise<IApplicantModel> {
+    return new Promise((resolve: any, reject: any) => {
+      ApplicantModel.find({ userType: userType }, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findAll(): Promise<IApplicantModel> {
+    return new Promise((resolve: any, reject: any) => {
+      ApplicantModel.find({}, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+}
--- recruit-inc-back/src/domain/model/IApplicantModel.ts
+++ recruit-inc-back/src/domain/model/IApplicantModel.ts
@@ -1,6 +1,17 @@
+import mongoose from 'mongoose';
+import { IGitModel } from './IGitModel';
 /**
  * Typegoose is including schema and model in one class
  */
 export interface IApplicantModel {
-  //TODO: Add field based on diagram
+  _id?: mongoose.Types.ObjectId;
+  platformUsername: string;
+  platformEmail: string;
+  iGit: IGitModel;
+  userType: UserType;
+}
+
+export enum UserType {
+    Applicant = 'Applicant',
+    Candidate = 'Candidate',
 }
