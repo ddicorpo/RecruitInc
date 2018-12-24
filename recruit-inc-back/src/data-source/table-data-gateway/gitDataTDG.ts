6f7832077446bb00e21a7877f4d04e75c97b0efd
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2018-12-24 09:43:21 -0500

    #21 Complete the TDG, Finder, schema and model for the gitData table.

--- recruit-inc-back/src/data-source/table-data-gateway/gitDataTDG.ts
+++ recruit-inc-back/src/data-source/table-data-gateway/gitDataTDG.ts
@@ -1,1 +1,40 @@
-//TODO
+import { BaseTDG } from './baseTDG';
+import { IGitDataModel } from '../../domain/model/IGitDataModel';
+import { GitDataModel } from '../schema/gitDataSchema';
+import { Types } from 'mongoose';
+
+export class GitDataTDG {
+  private baseTDG: BaseTDG;
+
+  constructor() {
+    this.baseTDG = new BaseTDG(GitDataModel);
+  }
+
+  public create(gitDataAttr: IGitDataModel): Promise<IGitDataModel> {
+    gitDataAttr._id = Types.ObjectId();
+    const newGitDataModel = new GitDataModel(gitDataAttr);
+
+    try {
+      return this.baseTDG.create(newGitDataModel, gitDataAttr);
+    } catch (Exception) {
+      throw new Error('Error while creating GitData');
+    }
+  }
+
+  public update(_id: string, updatedValue: IGitDataModel): Promise<boolean> {
+    try {
+      const GitDataModelToUpdate = new GitDataModel(updatedValue);
+      return this.baseTDG.update(Types.ObjectId(_id), GitDataModelToUpdate);
+    } catch (Exception) {
+      throw new Error('Error while creating GitData');
+    }
+  }
+
+  public delete(_id: string): Promise<boolean> {
+    try {
+      return this.baseTDG.delete(Types.ObjectId(_id));
+    } catch (Exception) {
+      throw new Error('Error while creating GitData');
+    }
+  }
+}
