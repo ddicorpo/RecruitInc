c325e48526b786ee8086252cfd7b89cd67846749
aghiles <aghiles_1@hotmail.com>
2018-12-29 11:32:04 -0500

    #21 added the finders

--- recruit-inc-back/src/data-source/finder/gitFinder.ts
+++ recruit-inc-back/src/data-source/finder/gitFinder.ts
@@ -1,1 +1,39 @@
-//TODO
+import { BaseFinder } from './baseFinder';
+import { GitModel } from '../schema/gitSchema';
+import { IGitModel } from '../../domain/model/IGitModel';
+import { Types } from 'mongoose';
+
+export class ApplicantFinder {
+  private baseFinder: BaseFinder;
+
+  constructor() {
+    this.baseFinder = new BaseFinder(GitModel);
+  }
+
+  public findById(_id: string): Promise<IGitModel> {
+    return new Promise((resolve: any, reject: any) => {
+      GitModel.findOne(Types.ObjectId(_id), (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findByToken(IToken: string): Promise<IGitModel> {
+    return new Promise((resolve: any, reject: any) => {
+      GitModel.findOne({ IToken: IToken }, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findAll(): Promise<IGitModel> {
+    return new Promise((resolve: any, reject: any) => {
+      GitModel.find({}, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+}
--- recruit-inc-back/src/data-source/finder/tokenFinder.ts
+++ recruit-inc-back/src/data-source/finder/tokenFinder.ts
@@ -1,1 +1,73 @@
-//TODO
+import { BaseFinder } from './baseFinder';
+import { TokenModel } from '../schema/tokenSchema';
+import { ITokenModel } from '../../domain/model/ITokenModel';
+import { Types } from 'mongoose';
+import { Platform } from '../../domain/model/ITokenModel';
+
+export class ApplicantFinder {
+  private baseFinder: BaseFinder;
+
+  constructor() {
+    this.baseFinder = new BaseFinder(TokenModel);
+  }
+
+  public findById(_id: string): Promise<ITokenModel> {
+    return new Promise((resolve: any, reject: any) => {
+      TokenModel.findOne(Types.ObjectId(_id), (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findByPlatform(platform: Platform): Promise<ITokenModel> {
+    return new Promise((resolve: any, reject: any) => {
+      TokenModel.find({ platform: platform }, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findByAccessToken(AccessToken: string): Promise<ITokenModel> {
+    return new Promise((resolve: any, reject: any) => {
+      TokenModel.findOne(
+        { AccessToken: AccessToken },
+        (error: any, obj: any) => {
+          if (error) reject(error.name + ': ' + error.message);
+          obj ? resolve(obj) : resolve();
+        }
+      );
+    });
+  }
+
+  public findByRefreshToken(RefreshToken: string): Promise<ITokenModel> {
+    return new Promise((resolve: any, reject: any) => {
+      TokenModel.findOne(
+        { RefreshToken: RefreshToken },
+        (error: any, obj: any) => {
+          if (error) reject(error.name + ': ' + error.message);
+          obj ? resolve(obj) : resolve();
+        }
+      );
+    });
+  }
+
+  public findByExpiryDate(ExpiryDate: string): Promise<ITokenModel> {
+    return new Promise((resolve: any, reject: any) => {
+      TokenModel.findOne({ ExpiryDate: ExpiryDate }, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+
+  public findAll(): Promise<ITokenModel> {
+    return new Promise((resolve: any, reject: any) => {
+      TokenModel.find({}, (error: any, obj: any) => {
+        if (error) reject(error.name + ': ' + error.message);
+        obj ? resolve(obj) : resolve();
+      });
+    });
+  }
+}
