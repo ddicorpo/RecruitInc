cc7443075bdd188a01da5d1a43a60319fe3e8570
aghiles <aghiles_1@hotmail.com>
2019-01-20 09:56:01 -0500

    #81 test for the cron tables

---
+++ recruit-inc-back/test/data-source/cronIntegration.test.ts
@@ -0,0 +1,69 @@
+import 'mocha';
+import { CronTDG } from '../../src/data-source/table-data-gateway/cronTDG';
+import { CronFinder } from '../../src/data-source/finder/CronFinder';
+import { expect } from 'chai';
+import * as mongoose from 'mongoose';
+import { ICronModel } from '../../src/domain/model/ICronModel';
+import { MongoConnectionFactory } from '../../src/data-source/db-registry/mongo/MongoConnectionFactory';
+
+require('dotenv').config(); //Get environment variables
+
+xdescribe('Test mongo cronObj', () => {
+  const cron: ICronModel = {
+    location: 'Montreal',
+    number_scanned: 456,
+    total_number: 75894,
+    cron_pattern: 'pattern1',
+    status: 'scanning',
+  };
+  const cron2: ICronModel = {
+    location: 'Quebec',
+    number_scanned: 456,
+    total_number: 754,
+    cron_pattern: 'pattern1',
+    status: 'scanning',
+  };
+  const cron3: ICronModel = {
+    location: 'Chicoutimi',
+    number_scanned: 777,
+    total_number: 1325,
+    cron_pattern: 'pattern1',
+    status: 'scanning',
+  };
+  const cronTDG: CronTDG = new CronTDG();
+  const cronFinder: CronFinder = new CronFinder();
+
+  before(() => {
+    // Establish connection
+    let myFactory: MongoConnectionFactory = new MongoConnectionFactory();
+    myFactory.defaultInitialization();
+    // Start connection
+    myFactory.getConnection();
+  });
+
+  after(() => {
+    mongoose.connection.close();
+    console.log(mongoose.connection.readyState);
+  });
+
+  it('Test mongo create cron object', async () => {
+    let createdCronobj: ICronModel = await cronTDG.create(cron);
+    expect(cron.location).to.equal(createdCronobj.location);
+    //Insert other gitObj
+    await cronTDG.create(cron2);
+    await cronTDG.create(cron3);
+  });
+
+  it('Test mongo findAll and then delete', async () => {
+    let cronObjFound: ICronModel;
+    //Find all gitObj
+    await cronFinder.findAll().then(doc => {
+      cronObjFound = doc;
+    });
+    //Delete the gitDatas that were found
+    for (let i: number = 0; i < 3; i++) {
+      let deleteSuccess: boolean = await cronTDG.delete(cronObjFound[i]._id);
+      expect(deleteSuccess).to.be.equal(true);
+    }
+  });
+});
