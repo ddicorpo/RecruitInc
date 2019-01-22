15bf897e8020057e5f197dcfeb843add71f34c99
ddicorpo <ddicorpo@gmail.com>
2019-01-22 16:28:59 -0500

    #117 Refactored a test to better check if shifting an array works
    Co-authored-by: 23jams <23jamesff@gmail.com>

--- recruit-inc-back/test/queues/repositoryQueue.test.ts
+++ recruit-inc-back/test/queues/repositoryQueue.test.ts
@@ -1,55 +1,58 @@
 import { expect } from 'chai';
 import { describe, it } from 'mocha';
 import { stubObject } from 'ts-sinon';
-import { RepositoryQueue } from "../../src/queue/queues/RepositoryQueue";
-import { RequiredClientInformation } from "../../src/queue/RequiredClientInformation";
-import { IGithubUser } from "../../src/data-extraction/github/api-entities/IGithubUser";
-
+import { RepositoryQueue } from '../../src/queue/queues/RepositoryQueue';
+import { RequiredClientInformation } from '../../src/queue/RequiredClientInformation';
+import { IGithubUser } from '../../src/data-extraction/github/api-entities/IGithubUser';
 
 const repoQueue: RepositoryQueue = RepositoryQueue.get_instance();
 
+describe.only('Get User information', () => {
+  before(() => {
+    const newUser: IGithubUser = {
+      login: 'bill nye',
+      createdAt: '',
+      url: '',
+      email: '',
+    };
+    let requiredInfo: RequiredClientInformation = new RequiredClientInformation(
+      newUser,
+      'testRepo',
+      'bob',
+      'path',
+      '122',
+      'montreal'
+    );
+    let requiredInfo2: RequiredClientInformation = new RequiredClientInformation(
+      newUser,
+      'testingStuff',
+      'bob',
+      'path',
+      '122',
+      'montreal'
+    );
+    repoQueue.enqueue(requiredInfo);
+    repoQueue.enqueue(requiredInfo2);
+  });
 
-xdescribe('Get User information', () => {
-    before(() => {
-        const newUser: IGithubUser = {
-            login: "bill nye",
-            createdAt: '',
-            url: '',
-            email: '',
-        };
-        let requiredInfo: RequiredClientInformation = new RequiredClientInformation(
-            newUser,
-            "testRepo",
-            "bob",
-            "path",
-            "122",
-            "montreal");
-        repoQueue.enqueue(requiredInfo);
-    });
+  it('Check to make sure enqueue worked, and size method works', () => {
+    let size = repoQueue.size();
+    expect(2).to.equal(size);
+  });
 
-    it('Check to make sure enqueue worked, and size method works', () => {
-        let size = repoQueue.size();
-        expect(1).to.equal(size);
-    });
+  it('Check to make sure is empty method works', () => {
+    let notEmpty = repoQueue.isEmpty();
+    expect(false).to.equal(notEmpty);
+  });
 
-    it('Check to make sure is empty method works', () => {
-        let notEmpty = repoQueue.isEmpty();
-        expect(false).to.equal(notEmpty);
-    });
-
-    it('Check to make sure dequeue works properly, needed to remove from global as well', () => {
-        let repoClient = repoQueue.dequeue();
-        expect("testRepo").to.equal(repoClient.prospect.repoName);
-        let size = repoQueue.size();
-        expect(0).to.equal(size);
-        let notEmpty = repoQueue.isEmpty();
-        expect(true).to.equal(notEmpty);
-    });
-
+  it('Check to make sure dequeue works properly, needed to remove from global as well', () => {
+    let repoClient = repoQueue.dequeue();
+    expect('testRepo').to.equal(repoClient.prospect.repoName);
+    let size = repoQueue.size();
+    expect(1).to.equal(size);
+    let repoClient2 = repoQueue.dequeue();
+    expect('testingStuff').to.equal(repoClient2.prospect.repoName);
+    let notEmpty = repoQueue.isEmpty();
+    expect(true).to.equal(notEmpty);
+  });
 });
-
-
-
-
-
-
