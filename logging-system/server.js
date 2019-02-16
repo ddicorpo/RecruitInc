3a28ad7d7942b06b09b4a1ca2c10a834d42fd6c1
winterhart <m.user.work@gmail.com>
2019-02-16 17:48:56 -0500

    Adding mongodb support
    Co-authored-by: philbeaudry <philippe.beaudry96@gmail.com>

--- logging-system/server.js
+++ logging-system/server.js
@@ -10,8 +10,30 @@ app.listen(5656, function () {
 });
 
 app.post('/', function(req, res){
-    var myData = req.body;
-    console.log(myData);
-    res.status(200).send();
+    var myData = JSON.stringify(req.body);   
+    sendLog(myData);
+
+    res.status(200).send(res);
 });
 
+
+function sendLog(myData){
+
+    const MongoClient = require('mongodb').MongoClient;
+    const uri = process.env.DB_URI;
+    const client = new MongoClient(uri, { useNewUrlParser: true });
+    
+    try{
+        client.connect(err => {
+            const collection = client.db("logging-system").collection("logs");
+
+            console.log(myData);
+            collection.save(JSON.parse(myData));
+
+            client.close();
+        });
+    }
+    catch(Exception){
+        console.log("Problem while sending log...")
+    }
+}
