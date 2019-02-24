6dfb02d737fc9e888c9d97c2b1a4962807c375bf
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2019-02-24 12:46:17 -0500

    #117 Refactor generalUpdate function

--- recruit-inc-back/src/data-source/table-data-gateway/githubUsersTDG.ts
+++ recruit-inc-back/src/data-source/table-data-gateway/githubUsersTDG.ts
@@ -48,50 +48,12 @@ export class GithubUsersTDG {
     update: any,
     options: any = {}
   ): Promise<boolean> {
-    return new Promise((resolve: any, reject: any) => {
-      try {
-        //mongoose.connection.db.command({
-        //    update: GithubUserModel.collection.name,
-        //    updates: [
-        //        { criteria: criteria, update: update, options: options}
-        //    ],
-        //})
-        mongoose.connection.db
-          .collection('githubUsers')
-          .updateOne(criteria, update, options);
-        this.baseTDG.logActionCompleted(this.generalUpdate.name);
-        resolve(true);
-      } catch (error) {
-        this.baseTDG.logActionFailure(
-          this.generalUpdate.name,
-          error.name,
-          error.toString()
-        );
-        resolve(false);
-      }
-    });
+    try {
+      return this.baseTDG.updateOne(criteria, update, options);
+    } catch (Exception) {
+      throw new Error(Exception.toString());
+    }
   }
-  //public generalUpdate(
-  //  criteria: any,
-  //  update: any,
-  //  options: any = {}
-  //): Promise<boolean> {
-  //  return new Promise((resolve: any, reject: any) => {
-  //    GithubUserModel.updateOne(criteria, update, options, (error, doc) => {
-  //      if (error) {
-  //        this.baseTDG.logActionFailure(
-  //          this.generalUpdate.name,
-  //          error.name,
-  //          error.toString()
-  //        );
-  //        resolve(false);
-  //      } else {
-  //        this.baseTDG.logActionCompleted(this.generalUpdate.name);
-  //        resolve(true);
-  //      }
-  //    });
-  //  });
-  //}
 
   public insertMany(githubUsers: IGithubUserModel[]): Promise<any> {
     return new Promise((resolve: any, reject: any) => {
