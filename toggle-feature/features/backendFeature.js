a94da01a236ef0f0238d40a539c20cab60d8767f
philbeaudry <philippe.beaudry96@gmail.com>
2019-02-24 16:30:26 -0500

    #77 Added backend features to be return when requested by backend

---
+++ toggle-feature/features/backendFeature.js
@@ -0,0 +1,34 @@
+class BackendFeatures{
+    constructor(){
+        this.backendFeatures = [
+            {
+                id: 'closedBeta',
+                name: 'This is a closed beta for only certain users',
+                description: 'Closed beta allows us to test on small groups of users',
+                enabled: false,
+                criteria: {isPaidUser: true, percentageOfUsers: 0.50}
+            },
+            {
+                id: 'newFeatureRollout',
+                name: 'A new feature rollout',
+                description: 'New feature done development, allows to gradually launch the new feature',
+                enabled: false
+                //criteria: // if criteria is put, no need for the "enabled"
+            },
+            {
+                id: 'experimentalFeature',
+                name: 'An Experimental Feature',
+                description: 'Experimental feature still in development, useful for internal development',
+                enabled: false,
+            },
+        ];
+
+    }
+
+    getBackendFeature() {
+        return this.backendFeatures;
+    }
+}
+
+module.exports = BackendFeatures;
+
--- toggle-feature/server.js
+++ toggle-feature/server.js
@@ -2,8 +2,14 @@ const express = require('express');
 const app = express();
 const port = 3547;
 
+const backendFeatures = require('./features/backendFeature');
+
 app.get('/', (req, res) => res.send('Hello World!'));
 app.get('/frontend', (req, res) => res.send('Front-end features!'));
-app.get('/backend', (req, res) => res.send('Back-end features'));
+
+app.get('/backend', function(req, res){
+    let features = new backendFeatures();
+    res.send(features.getBackendFeature());
+});
 
 app.listen(port, () => console.log(`toggle-feature is listening on port ${port}!`));
