78611dbc9188dd70bbe969861965537417dfb0bd
Mohamed Lemine <mohamedlemineelhadj@outlook.com>
2019-02-24 07:06:47 -0500

    #117 #81 modify generalUpdate function.

--- recruit-inc-back/src/data-source/table-data-gateway/githubUsersTDG.ts
+++ recruit-inc-back/src/data-source/table-data-gateway/githubUsersTDG.ts
@@ -2,6 +2,7 @@ import { BaseTDG } from './baseTDG';
 import { IGithubUserModel } from '../../domain/model/IGithubUserModel';
 import { GithubUserModel } from '../schema/githubUserSchema';
 import { Types, Model } from 'mongoose';
+import * as mongoose from 'mongoose';
 
 export class GithubUsersTDG {
   private baseTDG: BaseTDG;
@@ -48,21 +49,49 @@ export class GithubUsersTDG {
     options: any = {}
   ): Promise<boolean> {
     return new Promise((resolve: any, reject: any) => {
-      GithubUserModel.updateOne(criteria, update, options, (error, doc) => {
-        if (error) {
-          this.baseTDG.logActionFailure(
-            this.generalUpdate.name,
-            error.name,
-            error.toString()
-          );
-          resolve(false);
-        } else {
-          this.baseTDG.logActionCompleted(this.generalUpdate.name);
-          resolve(true);
-        }
-      });
+      try {
+        //mongoose.connection.db.command({
+        //    update: GithubUserModel.collection.name,
+        //    updates: [
+        //        { criteria: criteria, update: update, options: options}
+        //    ],
+        //})
+        mongoose.connection.db
+          .collection('githubUsers')
+          .updateOne(criteria, update, options);
+        this.baseTDG.logActionCompleted(this.generalUpdate.name);
+        resolve(true);
+      } catch (error) {
+        this.baseTDG.logActionFailure(
+          this.generalUpdate.name,
+          error.name,
+          error.toString()
+        );
+        resolve(false);
+      }
     });
   }
+  //public generalUpdate(
+  //  criteria: any,
+  //  update: any,
+  //  options: any = {}
+  //): Promise<boolean> {
+  //  return new Promise((resolve: any, reject: any) => {
+  //    GithubUserModel.updateOne(criteria, update, options, (error, doc) => {
+  //      if (error) {
+  //        this.baseTDG.logActionFailure(
+  //          this.generalUpdate.name,
+  //          error.name,
+  //          error.toString()
+  //        );
+  //        resolve(false);
+  //      } else {
+  //        this.baseTDG.logActionCompleted(this.generalUpdate.name);
+  //        resolve(true);
+  //      }
+  //    });
+  //  });
+  //}
 
   public insertMany(githubUsers: IGithubUserModel[]): Promise<any> {
     return new Promise((resolve: any, reject: any) => {
