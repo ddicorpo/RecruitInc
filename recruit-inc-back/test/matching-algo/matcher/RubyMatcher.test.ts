338c93b41a5bc9abfc4fa72e5d69e43cb73bdf62
momo2793 <mohamed_2_27@hotmail.com>
2019-02-27 23:47:00 -0500

    import files for Ruby test

---
+++ recruit-inc-back/test/matching-algo/matcher/RubyMatcher.test.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+import 'mocha';
+import { MatcherClient } from '../../../src/matching-algo/matcher-client/MatcherClient';
+import { AbstractLanguageMatcher } from '../../../src/matching-algo/matcher/AbstractLanguageMatcher';
+import { IGitProjectSummary } from '../../../src/matching-algo/data-model/output-model/IGitProjectSummary';
+import { IGitProjectOutput } from '../../../src/matching-algo/data-model/output-model/IGitProjectOutput';
+import { CsharpMatcher } from '../../../src/matching-algo/matcher/Csharp/csharpMatcher';
