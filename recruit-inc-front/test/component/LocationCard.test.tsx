203443641ed4eb7bfa207b07f95e10299305d9db
winterhart <m.user.work@gmail.com>
2019-02-28 16:54:31 -0500

    #152 adding full testing of the card component

---
+++ recruit-inc-front/test/component/LocationCard.test.tsx
@@ -0,0 +1,39 @@
+import { LocationWatch } from '../../src/app/model/Location/LocationWatch';
+import { LocationCard } from '../../src/app/components/Location/LocationCard';
+import { shallow } from 'enzyme';
+import * as React from 'react';
+
+describe('LocationCard Component Testing', () => {
+  it('Testing Component with data', () => {
+    const fakeWatchLocation: LocationWatch = new LocationWatch('bob', 200, 30);
+    const props = {
+      locationWatchData: fakeWatchLocation,
+    };
+
+    const wrapper = shallow(<LocationCard {...props} />);
+    const itShallContains: boolean = wrapper.contains(fakeWatchLocation.name);
+    expect(itShallContains).toBe(true);
+  });
+
+  it('Testing Component full list', () => {
+    const fakeWatchLocation: LocationWatch = new LocationWatch('bob', 200, 200);
+    const props = {
+      locationWatchData: fakeWatchLocation,
+    };
+
+    const wrapper = shallow(<LocationCard {...props} />);
+    const itShallContains: boolean = wrapper.contains('Finished');
+    expect(itShallContains).toBe(true);
+  });
+
+  it('Testing Component non-full list', () => {
+    const fakeWatchLocation: LocationWatch = new LocationWatch('bob', 200, 20);
+    const props = {
+      locationWatchData: fakeWatchLocation,
+    };
+
+    const wrapper = shallow(<LocationCard {...props} />);
+    const itShallContains: boolean = wrapper.contains('Running');
+    expect(itShallContains).toBe(true);
+  });
+});
