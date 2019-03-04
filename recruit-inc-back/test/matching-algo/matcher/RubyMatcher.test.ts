b16f5ccab47e79adc07a663e8ef288e661761f6e
momo2793 <mohamed_2_27@hotmail.com>
2019-03-03 20:30:05 -0500

    #77 tests completed for Ruby

--- recruit-inc-back/test/matching-algo/matcher/RubyMatcher.test.ts
+++ recruit-inc-back/test/matching-algo/matcher/RubyMatcher.test.ts
@@ -9,3 +9,30 @@ import { RubyMatcher } from '../../../src/matching-algo/matcher/Ruby/RubyMatcher
 //setting up the language matcher
 const rubyMatcher: AbstractLanguageMatcher = new RubyMatcher();
 const customLanguageMatchers: AbstractLanguageMatcher[] = [rubyMatcher];
+
+describe('Test Ruby matching algorithm', () => {
+  it('The Ruby matcher should return the correct output object with Ruby repository', () => {
+    // GIVEN
+    const input = require('./input/ruby.json');
+    const expected = require('./expected-output/ruby.out.json');
+    const client: MatcherClient = new MatcherClient(
+      input,
+      customLanguageMatchers
+    );
+
+    // WHEN
+    const computedOutput: IGitProjectSummary = client.execute();
+
+    // THEN
+    const length: number = computedOutput.projectsOutput.length;
+    for (let index = 0; index < length; ++index) {
+      const computedProjectSum: IGitProjectOutput =
+        computedOutput.projectsOutput[index];
+      if (JSON.stringify(expected) !== JSON.stringify(computedProjectSum)) {
+        console.log('expected : ' + JSON.stringify(expected));
+        console.log('actual: ' + JSON.stringify(computedProjectSum));
+        expect.fail();
+      }
+    }
+  });
+});
