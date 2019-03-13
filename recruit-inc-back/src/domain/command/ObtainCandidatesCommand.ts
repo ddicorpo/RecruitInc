47cb6389968e2c5f9f39b3bacaa007661f2c8862
momo2793 <mohamed_2_27@hotmail.com>
2019-03-13 04:10:36 -0400

    #155 technologies used query test for filtering purposes

--- recruit-inc-back/src/domain/command/ObtainCandidatesCommand.ts
+++ recruit-inc-back/src/domain/command/ObtainCandidatesCommand.ts
@@ -1,44 +1,53 @@
 import { AbstractCommand } from './AbstractCommand';
 import { RequestContext } from './RequestContext';
-import { ApplicantFinder } from '../../data-source/finder/ApplicantFinder'
+import { ApplicantFinder } from '../../data-source/finder/ApplicantFinder';
 import { IApplicantModel } from '../model/IApplicantModel';
 
 // Usually you want to go through a factory/Data Mapper model
 // This is a temporary command
 
 export class ObtainCandidatesCommand extends AbstractCommand {
+  private finder: ApplicantFinder = new ApplicantFinder();
+  constructor(applicationContext?: RequestContext) {
+    super();
+  }
+  public async getCandidates(page: number, filter: string): Promise<any> {
+    try {
+      //Build query from filter...
+      //TODO: Handle technologies... e.g. Java, Python
 
-    private finder : ApplicantFinder = new ApplicantFinder();
-    constructor(applicationContext?: RequestContext) {
-        super();
+      const query = {
+        'iGit.iGitData.0.gitProjectSummary.totalOutput': {
+          $elemMatch: {
+            languageOrFramework: filter,
+            linesOfCode: { $gt: 0 },
+            numberOfCommits: { $gt: 0 },
+            frameworks: [],
+          },
+        },
+      };
+
+      //Prevent crash of the application by inserting page=-1 or page=0
+      if (page < 1) {
+        page = 1;
+      }
+      //TODO Build the query based on the filter...
+      let allCandidates: IApplicantModel = await this.finder.findByPageQuery(
+        query,
+        page
+      );
+      return JSON.stringify(allCandidates);
+    } catch (CommandException) {
+      throw CommandException;
     }
-    public async getCandidates(page: number, filter : string): Promise<any> {
-        try{
-            //Build query from filter...
-            //TODO: Handle technologies... e.g. Java, Python
-            const query = undefined;
+  }
 
-            //Prevent crash of the application by inserting page=-1 or page=0
-            if(page < 1){
-                page = 1
-            }
-            //TODO Build the query based on the filter...
-            let allCandidates : IApplicantModel = await this.finder.findByPageQuery(query, page);
-            return JSON.stringify(allCandidates);
-        }catch(CommandException){
-            throw CommandException
-        }
+  public async getAllCandidates(): Promise<any> {
+    try {
+      let allCandidates: IApplicantModel = await this.finder.findAll();
+      return JSON.stringify(allCandidates);
+    } catch (CommandException) {
+      throw CommandException;
     }
-
-    public async getAllCandidates(): Promise<any>{
-        try {
-            let allCandidates: IApplicantModel = await this.finder.findAll();
-            return JSON.stringify(allCandidates);
-
-        }catch(CommandException){
-            throw CommandException
-        }
-
-    }
-
+  }
 }
