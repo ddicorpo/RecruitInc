df3b0bfec6c0af70b18f087725342fc84d19d763
rena-21 <aghiles_1@hotmail.com>
2019-03-15 15:58:34 -0400

    #44 added the page direct messaging

---
+++ recruit-inc-front/src/app/pages/DirectMessaging.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react';
+
+class DirectMessaging extends React.Component<any, any> {
+  render() {
+    return (
+      <div className="container-fluid">
+        <div className="page-header">
+          <h2 className="header-title">Direct Messaging</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default DirectMessaging;
