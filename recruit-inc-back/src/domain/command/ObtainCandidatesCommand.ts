8cff9e4e81345791dae278ed715ae306b396396d
BenjaminTanguay <benjamin.tanguay@gmail.com>
2019-03-16 23:19:14 -0400

    #155 Transformed the string query to something mongoose understands

--- recruit-inc-back/src/domain/command/ObtainCandidatesCommand.ts
+++ recruit-inc-back/src/domain/command/ObtainCandidatesCommand.ts
@@ -7,10 +7,12 @@ import { IApplicantModel } from '../model/IApplicantModel';
 // This is a temporary command
 
 export class ObtainCandidatesCommand extends AbstractCommand {
-  private baseQuery: String = 'db.applicants.find(';
-
-  private queryExclude: String =
-    '{"iGit.IGitData.dataEntry.projectInputs.applicantCommits": 0, "iGit.IGitData.dataEntry.projectInputs.projectStructure": 0, "iGit.IGitData.dataEntry.projectInputs.downloadedSourceFile": 0, "iGit.IToken": 0}';
+  private queryExclude: {} = {
+    'iGit.IGitData.dataEntry.projectInputs.applicantCommits': 0,
+    'iGit.IGitData.dataEntry.projectInputs.projectStructure': 0,
+    'iGit.IGitData.dataEntry.projectInputs.downloadedSourceFile': 0,
+    'iGit.IToken': 0,
+  };
 
   private finder: ApplicantFinder = new ApplicantFinder();
   constructor(applicationContext?: RequestContext) {
@@ -18,16 +20,16 @@ export class ObtainCandidatesCommand extends AbstractCommand {
   }
   public async getCandidates(page: number, filters: string[]): Promise<any> {
     try {
-      const query: string = this.getQuery(filters);
+      const query: {} = this.getQuery(filters);
 
       //Prevent crash of the application by inserting page=-1 or page=0
       if (page < 1) {
         page = 1;
       }
-      //TODO Build the query based on the filter...
       let allCandidates: IApplicantModel = await this.finder.findByPageQuery(
         query,
-        page
+        page,
+        this.queryExclude
       );
       return JSON.stringify(allCandidates);
     } catch (CommandException) {
@@ -67,27 +69,30 @@ export class ObtainCandidatesCommand extends AbstractCommand {
     }
   }
 
-  private getQuery(filters: string[]): string {
-    let findQuery: string =
-      filters.length > 0
-        ? '{"iGit.IGitData.gitProjectSummary.totalOutput": {\'$all\': ['
-        : '{}';
-    const filtersMatch: string[] = [];
+  private getQuery(filters: string[]): {} {
+    const filtersMatch = [];
     for (const filter of filters) {
       filtersMatch.push(this.generateFilterMatch(filter));
     }
-    if (filters.length > 0) {
-      findQuery += filtersMatch.join(', ') + ']}}';
-    }
 
-    return this.baseQuery + findQuery + ', ' + this.queryExclude + ')';
+    let findQuery: {} =
+      filters.length > 0
+        ? {
+            'iGit.IGitData.gitProjectSummary.totalOutput': {
+              $all: filtersMatch,
+            },
+          }
+        : {};
+
+    return findQuery;
   }
 
-  private generateFilterMatch(language: string): string {
-    return (
-      '{$elemMatch: {languageOrFramework: "' +
-      language +
-      '", "$and": [ {linesOfCode: {$gt: 0}}, {numberOfCommits: {$gt : 0} }] }}'
-    );
+  private generateFilterMatch(language: string) {
+    return {
+      $elemMatch: {
+        languageOrFramework: language,
+        $and: [{ linesOfCode: { $gt: 0 } }, { numberOfCommits: { $gt: 0 } }],
+      },
+    };
   }
 }
