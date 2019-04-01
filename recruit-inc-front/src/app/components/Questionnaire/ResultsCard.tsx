35dd33702b513ceebaf345103a671e60be7f61f5
ddicorpo <ddicorpo@gmail.com>
2019-04-01 19:48:04 -0400

    #165 WIP front end

---
+++ recruit-inc-front/src/app/components/Questionnaire/ResultsCard.tsx
@@ -0,0 +1,6 @@
+//import * as React from 'react';
+//import { Logger } from '../Logger';
+
+export interface IResultsCardProps {
+
+}
--- recruit-inc-front/src/app/pages/CandidateQuestionnaire.tsx
+++ recruit-inc-front/src/app/pages/CandidateQuestionnaire.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react';
+// import Select from 'react-select';
 import { ObtainQuestionnaireCandidates } from "../services/ObtainQuestionnaireCandidates";
 import { IQuestionnaireCandidate } from "../model/QuestionnaireCandidate/IQuestionnaireCandidate";
 import { QuestionnaireAdapter } from "../adapter/QuestionnaireCandidate/QuestionnaireAdapter";
-
+// import Pagination from 'react-js-pagination';
 // import { Logger } from "../Logger";
 
 class CandidateQuestionnaire extends React.Component<any, any> {
@@ -18,7 +19,7 @@ class CandidateQuestionnaire extends React.Component<any, any> {
         let questionnaireService: ObtainQuestionnaireCandidates = new ObtainQuestionnaireCandidates();
 
         questionnaireService.changePage(page);
-        this.setState({ activePage: page });
+        this.setState({activePage: page});
 
         if (isSearchFilter) {
             console.log(this.state.selectedTechOptions);
@@ -46,7 +47,49 @@ class CandidateQuestionnaire extends React.Component<any, any> {
 
     render() {
         return (
-            <h1>Hello World</h1>
+            <div className="container-fluid">
+                <div className="page-header">
+                    <h2 className="header-title">Questionnaire Results</h2>
+                </div>
+                <div className="card">
+                    <div className="card-header border bottom">
+                        <h4 className="card-title">Search Candidate</h4>
+                    </div>
+                    <div className="card-body">
+                        <div className="row m-t-30">
+                            <div className="col-md-4">
+                                <div className="p-h-10">
+                                    <div className="form-group">
+                                        <label className="control-label">&nbsp;</label>
+                                        <input type="text" className="form-control" placeholder="Search..."/>
+                                    </div>
+                                </div>
+                            </div>
+                            <div className="col-md-4">
+                                <div className="p-h-10">
+                                    <div className="form-group">
+                                        <label className="control-label">&nbsp;</label>
+                                        <button
+                                            id="save"
+                                            className="btn btn-success form-control super-button"
+                                            type="button"
+                                        >
+                                            Search
+                                        </button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="card">
+                    <div className="card-header border bottom">
+                        <h4 className="card-title">Results</h4>
+                    </div>
+                </div>
+            </div>
+
         )
     }
 };
