06cf53f48e5056ca27562f610b29307d92528524
philbeaudry <philippe.beaudry96@gmail.com>
2019-04-01 16:02:06 -0400

    #175 Added the retrieval of the feature value in the backend from the toggle service.

--- recruit-inc-back/src/toggle-feature/ToggleFeature.ts
+++ recruit-inc-back/src/toggle-feature/ToggleFeature.ts
@@ -3,15 +3,33 @@ import fetch from 'node-fetch';
 let fflip = require('fflip');
 
 export class ToggleFeature {
+
+  private _newFeatureRollout: boolean = false;
+  private _cronActive: boolean = false;
+
+  get newFeatureRollout(): boolean {
+    return this._newFeatureRollout;
+  }
+
+  get cronActive(): boolean {
+    return this._cronActive;
+  }
+
   public async retrieveToggleFeature(): Promise<void> {
     console.log('Retrieving toggle feature configs ');
     fflip.config({
       criteria: await this.getCriteria(),
       features: await this.getFeature(),
     });
-    //Toggle enabled for new feature rollout. (example)
-    if (fflip.features.newFeatureRollout.enabled) {
+    //Toggle enabled for new feature rollout.
+    if (fflip.features._newFeatureRollout.enabled) {
+      this._newFeatureRollout = true;
       console.log('New Feature Rollout is Enabled');
+    }
+    //Feature is retrieved to see if the cron job should run
+    if (fflip.features._cronActive.enabled) {
+      this._cronActive = true;
+      console.log('Cron job is activated using feature toggle "cronActive"');
     }
   }
 
