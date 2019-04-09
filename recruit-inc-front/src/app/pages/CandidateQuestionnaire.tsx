b409d5570a722acfa71ee2dfea2a815cdc140fdb
ddicorpo <ddicorpo@gmail.com>
2019-04-08 21:54:59 -0400

    #165 removed ^ from package.json. Removed commented code in front end. Added .env.example in front-end-questionnaire directory

--- recruit-inc-front/src/app/pages/CandidateQuestionnaire.tsx
+++ recruit-inc-front/src/app/pages/CandidateQuestionnaire.tsx
@@ -59,38 +59,6 @@ class CandidateQuestionnaire extends React.Component<any, any> {
                 <div className="page-header">
                     <h2 className="header-title">Questionnaire Results</h2>
                 </div>
-                {/*<div className="card">*/}
-                    {/*<div className="card-header border bottom">*/}
-                        {/*<h4 className="card-title">Search Candidate</h4>*/}
-                    {/*</div>*/}
-                    {/*<div className="card-body">*/}
-                        {/*<div className="row m-t-30">*/}
-                            {/*<div className="col-md-6">*/}
-                                {/*<div className="p-h-10">*/}
-                                    {/*<div className="form-group">*/}
-                                        {/*<label className="control-label">&nbsp;</label>*/}
-                                        {/*<input type="text" className="form-control" placeholder="Search..."/>*/}
-                                    {/*</div>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-                            {/*<div className="col-md-6">*/}
-                                {/*<div className="p-h-10">*/}
-                                    {/*<div className="form-group">*/}
-                                        {/*<label className="control-label">&nbsp;</label>*/}
-                                        {/*<button*/}
-                                            {/*id="save"*/}
-                                            {/*className="btn btn-success form-control super-button"*/}
-                                            {/*type="button"*/}
-                                        {/*>*/}
-                                            {/*Search*/}
-                                        {/*</button>*/}
-                                    {/*</div>*/}
-                                {/*</div>*/}
-                            {/*</div>*/}
-                        {/*</div>*/}
-                    {/*</div>*/}
-                {/*</div>*/}
-
                 <div className="card">
                     <div className="card-header border bottom">
                         <h4 className="card-title">Results</h4>
